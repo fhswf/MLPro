7fe75920a5457d10bcec45713aa161e79fe4142c
Arend <arend.detlef@fh-swf.de>
2021-09-12 18:56:27 +0200

    Transferred from local GitLab

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='mlpro',
+version='0.0.0',
+description='Machine Learning Professional',
+author='Detlef Arend',
+author_mail='detlef@arend-privat.net',
+license='MIT',
+packages=['mlpro'],
+zip_safe=False)
