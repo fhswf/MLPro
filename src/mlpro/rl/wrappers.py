af014914639f286041a14967fb15ed4fb409ec95
steveyuwono <steveyuwono@gmail.com>
2021-09-23 14:46:12 +0200

    Update WrEnvGym and WrEnvPZoo class following big data extension

--- src/mlpro/rl/wrappers.py
+++ src/mlpro/rl/wrappers.py
@@ -7,12 +7,14 @@
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
 ## -- 2021-08-27  0.0.0     SY       Creation
 ## -- 2021-08-27  1.0.0     SY       Release of first version
-## --                                New classes WrEnvPZoo, WrPolicyRAY
+## --                                New classes WrEnvPZoo
 ## -- 2021-09-11  1.0.0     MRD      Change Header information to match our new library name
+## -- 2021-09-23  1.1.0     SY       Update WrEnvGym to solve big data issues
+## --                                WrEnvPZoo is ready to use
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 1.0.0 (2021-08-27)
+Ver. 1.1.0 (2021-09-23)
 
 This module provides wrapper classes for reinforcement learning tasks.
 """
@@ -78,13 +80,8 @@ class WrEnvGym(Environment):
 ## -------------------------------------------------------------------------------------------------
     def _recognize_space(self, p_gym_space) -> ESpace:
         space = ESpace()
-
-        if len(p_gym_space.shape) == 0:
-            space.add_dim(Dimension(p_id=0,p_name_short='0'))
-        else:
-            for d in range(p_gym_space.shape[0]):
-                space.add_dim(Dimension(p_id=d, p_name_short=str(d), p_boundaries=[p_gym_space.low[d], p_gym_space.high[d]]))
-
+        space.add_dim(Dimension(p_id=0,p_name_short='0', p_base_set='DO'))
+        
         return space
 
 
@@ -99,10 +96,11 @@ class WrEnvGym(Environment):
 
         # 1 Reset Gym environment and determine initial state
         observation = self._gym_env.reset()
+        obs         = DataObject(observation)
 
         # 2 Create state object from Gym observation
-        state = State(self._state_space)
-        state.set_values(observation)
+        state   = State(self._state_space)
+        state.set_values(obs.get_data())
         self._set_state(state)
 
 
@@ -123,10 +121,11 @@ class WrEnvGym(Environment):
 
         # 2 Process step of Gym environment
         observation, reward_gym, self.done, info = self._gym_env.step(action_gym)
+        obs     = DataObject(observation)
 
         # 3 Create state object from Gym observation
-        state = State(self._state_space)
-        state.set_values(observation)
+        state   = State(self._state_space)
+        state.set_values(obs.get_data())
         self._set_state(state)
 
         # 4 Create and store reward object
@@ -210,30 +209,14 @@ class WrEnvPZoo(Environment):
     def _recognize_space(self, p_zoo_space, dict_name) -> ESpace:
         space = ESpace()
         id_ = 0
-        for k in p_zoo_space:
-            try:
-                if p_zoo_space[k].shape[0] == 0:
-                    space.add_dim(Dimension(p_id=0,p_name_short='0'))
-                else:
-                    dim = len(p_zoo_space[k].shape)
-                    count = 1
-                    for d in range(dim):
-                        count = count*p_zoo_space[k].shape[d]
-                        
-                    for c in range(count):
-                        space.add_dim(Dimension(p_id=id_, p_name_short=k+"_"+str(c), p_boundaries=[np.concatenate(p_zoo_space[k].low, axis=None)[c], np.concatenate(p_zoo_space[k].high, axis=None)[c]]))
-                        id_ += 1
-            except:
-                p_zoo_space = self._zoo_env.observation_spaces
-                dim = len(p_zoo_space[k][dict_name].shape)
-                count = 1
-                for d in range(dim):
-                    count = count*p_zoo_space[k][dict_name].shape[d]
-                    
-                for c in range(count):
-                    space.add_dim(Dimension(p_id=id_, p_name_short=k+"_"+str(c), p_boundaries=[np.concatenate(p_zoo_space[k][dict_name].low, axis=None)[c], np.concatenate(p_zoo_space[k][dict_name].high, axis=None)[c]]))
-                    id_ += 1
-                pass
+        
+        if dict_name == "observation":
+            space.add_dim(Dimension(p_id=0,p_name_short='0', p_base_set='DO'))
+        elif dict_name == "action_mask":
+            for k in p_zoo_space:
+                space.add_dim(Dimension(p_id=id_,p_name_short=k, p_base_set='DO'))
+                id_ += 1
+                
         return space
 
 ## -------------------------------------------------------------------------------------------------
@@ -245,19 +228,18 @@ class WrEnvPZoo(Environment):
     def reset(self):
         self.log(self.C_LOG_TYPE_I, 'Reset')
         
-        for k in self._zoo_env.action_spaces:
-            # 1 Reset Zoo environment and determine initial state
-            self._zoo_env.reset()
-            observation, _, _, _ = self._zoo_env.last()
-            try:
-                observation = np.concatenate(observation, axis=None)
-            except:
-                observation = np.concatenate(observation['observation'], axis=None)
-            
-            # 2 Create state object from Zoo observation
-            state = State(self._state_space)
-            state.set_values(observation)
-            self._set_state(state)
+        # 1 Reset Zoo environment and determine initial state
+        self._zoo_env.reset()
+        observation, _, _, _ = self._zoo_env.last()
+        obs     = DataObject(observation)
+        
+        # 2 Create state object from Zoo observation
+        state   = State(self._state_space)
+        if isinstance(observation, dict):
+            state.set_values(obs.get_data()['observation'])
+        else:
+            state.set_values(obs.get_data())
+        self._set_state(state)
 
 
 ## -------------------------------------------------------------------------------------------------
@@ -277,22 +259,25 @@ class WrEnvPZoo(Environment):
     
             action_zoo = action_sorted_agent.astype(self._zoo_env.action_spaces[k].dtype)
             
-        # 2 Process step of Zoo environment
+        # 2 Process step of Zoo environment that automatically switches control to the next agent.
             observation, reward_zoo, self.done, info = self._zoo_env.last()
-            try:
-                observation = np.concatenate(observation, axis=None)
-            except:
-                observation = np.concatenate(observation['observation'], axis=None)
+            obs     = DataObject(observation)
             
             if self.done:
                 self._zoo_env.step(None)
             else:
-                self._zoo_env.step(np.atleast_1d(action_zoo))
+                try:
+                    self._zoo_env.step(action_zoo)
+                except:
+                    self._zoo_env.step(np.atleast_1d(action_zoo))
             agent_num += 1
 
         # 3 Create state object from Zoo observation
             state = State(self._state_space)
-            state.set_values(observation)
+            if isinstance(observation, dict):
+                state.set_values(obs.get_data()['observation'])
+            else:
+                state.set_values(obs.get_data())
             self._set_state(state)
 
         # 4 Create and store reward object
