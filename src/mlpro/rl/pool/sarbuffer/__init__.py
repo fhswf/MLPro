be1d1c73086a59c35b78e1fa754d1454ccc418db
Mochammad Rizky Diprasetya <diprasetya.rizky@pm.me>
2021-09-26 21:04:05 +0200

    Restructure

--- src/mlpro/rl/pool/sarbuffer/__init__.py
+++ src/mlpro/rl/pool/sarbuffer/__init__.py
@@ -1,1 +1,2 @@
-from mlpro.rl.pool.sarbuffer.randomsarbuffer import RandomSARBuffer
+from mlpro.rl.pool.sarbuffer.RandomSARBuffer import RandomSARBuffer
+from mlpro.rl.pool.sarbuffer.PrioritizedBuffer import PrioritizedBuffer
