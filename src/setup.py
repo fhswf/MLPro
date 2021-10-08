32c5f92e4b63b5a4ce4d814aca89535ee744edfd
Arend <arend.detlef@fh-swf.de>
2021-10-08 18:13:27 +0200

    Updated setup files and created a build

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlpro',
-version='0.0.0',
+version='0.5.0',
 description='Machine Learning Professional',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
