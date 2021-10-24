a6933fcd36b930ef7d6b585b5983138624ac02ed
Mochammad Rizky Diprasetya <diprasetya.rizky@pm.me>
2021-10-24 19:28:31 +0200

    adjust howto

--- examples/rl/Howto 10 - (RL) Train using SB3 Wrapper.py
+++ examples/rl/Howto 10 - (RL) Train using SB3 Wrapper.py
@@ -52,6 +52,7 @@ class MyScenario(Scenario):
         policy_sb3 = PPO(
                     policy="MlpPolicy", 
                     env=None,
+                    n_steps=500,
                     _init_setup_model=False)
 
         # DQN Discrete only
@@ -77,7 +78,6 @@ class MyScenario(Scenario):
                 p_sb3_policy=policy_sb3, 
                 p_observation_space=self._env.get_state_space(),
                 p_action_space=self._env.get_action_space(),
-                p_buffer_size=500,
                 p_ada=p_ada,
                 p_logging=p_logging)
         
