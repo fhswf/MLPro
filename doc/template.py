f35a87e66703446d0d89e9a84abaefe219a80e39
William Budiatmadjaja <william.budiatmadjaja@gmail.com>
2021-12-01 09:32:07 +0100

    Template Draft Update #11

--- doc/template.py
+++ doc/template.py
@@ -29,20 +29,81 @@ This module provides ...
 ## -------------------------------------------------------------------------------------------------
 class Example:
     """
-    ...
+    The __init__ method should be documented in the class level docstring and the docstring itself
+    should not go beyond 100 characters length (within the dash separator). Sections inside the 
+    docstring can be seperated like the reStructuredText format.
+    
+    Parameters are documented in the Parameters section.
+    Public attributes of classes are documented inisde Attributes section.
+    Returns attronites are documented in the Returns section.
+    
+    .. _Further_formatting_information: 
+        https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_numpy.html#example-numpy
+        https://numpydoc.readthedocs.io/en/latest/format.html
+    
+    Notes
+    -----
+        The content inside the section should be indented. 
+    
+    Parameters
+    ----------
+    p_arg1 : int
+        explanation of the first parameter.
+    p_arg2 : bool
+        explanation of the second parameter.
+        
+    Attributes
+    ----------
+    attr1: int
+        explanation of the public attribute attr1.
 
     """
 
 ## -------------------------------------------------------------------------------------------------
     def __init__(self, p_arg1, p_arg2):
+        self.attr1 = p_arg1
         pass
 
 
 ## -------------------------------------------------------------------------------------------------
     def example_method(self, p_arg1):
         """
+        Example on how to document return type. 
+        
+        Notes
+        -----
+            The name of the return value is optional, but the type is always 
+            required. 
+        
+        Parameters
+        ----------
+        p_arg1 : int
+            explanation of the first parameter.
+                
+        Returns
+        -------
+        int
+            Description of the returned value.
         """
-        pass
+        return p_arg1
+        
+
+## -------------------------------------------------------------------------------------------------
+    def example_method_no_return(self, p_arg1):
+        """
+        Example on how to document return type. 
+        
+        Notes
+        -----
+            When there is no item to be returned, the return section is omitted.
+        
+        Parameters
+        ----------
+        p_arg1 : int
+            explanation of the first parameter.
+                
+        """
+        return 
 
 
 
