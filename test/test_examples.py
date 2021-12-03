a079e10a1636871d70bc7dd5a56d5c35cd6fb369
Mochammad Rizky Diprasetya <diprasetya.rizky@pm.me>
2021-12-03 21:49:35 +0100

    Fix How to 13 and include how to 15 to the test

--- test/test_examples.py
+++ test/test_examples.py
@@ -36,6 +36,8 @@ howto_list = {
     "rl_10": "examples.rl.Howto 10 - (RL) Train using SB3 Wrapper",
     "rl_11": "examples.rl.Howto 11 - (RL) Wrap mlpro Environment class to gym environment",
     "rl_12": "examples.rl.Howto 12 - (RL) Wrap mlpro Environment class to petting zoo environment",
+    "rl_13": "examples.rl.Howto 13 - (RL) Comparison Native and Wrapper SB3 Policy",
+    "rl_15": "examples.rl.Howto 15 - (RL) Train Robothtm with SB3 Wrapper",
     "sciui_01": "examples.sciui.Howto 01 (SciUI) - Reuse of interactive 2D,3D input space"
 }
 
