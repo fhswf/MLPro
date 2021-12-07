1c0f68fe8e96d34ab8f39bbffb5d18d0825034f3
steveyuwono <steveyuwono@gmail.com>
2021-12-07 08:17:54 +0100

    hyperopt: add setup spaces method

--- src/mlpro/wrappers/hyperopt.py
+++ src/mlpro/wrappers/hyperopt.py
@@ -16,6 +16,7 @@ This module provides a wrapper class for hyperparameter tuning by reusinng Hyper
 
 from hyperopt import *
 from mlpro.bf.ml import *
+from mlpro.bf.math import *
 
 
 
@@ -76,7 +77,7 @@ class WrHPTHyperopt(HyperParamTuner):
     #     return self._maximize()
 
 ## -------------------------------------------------------------------------------------------------
-    def maximize(self, p_ofct, p_model:Model, p_num_trials, p_algo=C_ALGO_RAND) -> TrainingResults:
+    def maximize(self, p_ofct, p_model:Model, p_num_trials, p_algo=C_ALGO_RAND, p_ids=None) -> TrainingResults:
         """
         ...
 
@@ -88,8 +89,10 @@ class WrHPTHyperopt(HyperParamTuner):
             Model object to be tuned.
         p_num_trials : int    
             Number of trials
-        p_algo : str    
-            Selection of a hyperparameter tuning algorithm
+        p_algo : str, optional    
+            Selection of a hyperparameter tuning algorithm, default: C_ALGO_RAND
+        p_algo : list of str, optional
+            List of hyperparameter ids to be tuned, otherwise all hyperparameters, default: None
 
         Returns
         -------
@@ -100,18 +103,19 @@ class WrHPTHyperopt(HyperParamTuner):
         super().maximize(p_ofct, p_model, p_num_trials)
 
         self._algo          = p_algo
+        self._ids           = p_ids
         return self._maximize()
 
 
 ## -------------------------------------------------------------------------------------------------
     def _maximize(self) -> TrainingResults:
-        space       = self.SetupSpaces()
+        spaces              = self.SetupSpaces()
         if self._algo == 'TPE':
-            self.algo = tpe.suggest()
+            self.algo       = tpe.suggest()
         elif self._algo == 'RND':
-            self.algo = rand.suggest()
+            self.algo       = rand.suggest()
             
-        best        = fmin(self.Objective, space, self.algo, self._num_trials, trials=Trials())
+        best                = fmin(self.Objective, spaces, self.algo, self._num_trials, trials=Trials())
         raise NotImplementedError
 
 ## -------------------------------------------------------------------------------------------------
@@ -124,7 +128,39 @@ class WrHPTHyperopt(HyperParamTuner):
 
 ## -------------------------------------------------------------------------------------------------
     def SetupSpaces(self):
-        # 1. setup boundaries
+        """
+        This method is used to setup the hyperparameter spaces, including the tuning boundaries and a suitable discrete value.
+        The hyperparameter should be bounded both above and below.
+        We are using the "quantized" continuous distributions for natural and integer numbers.
+        Meanwhile the real numbers are not quantized.
+        For different parameter expressions, please redefined this method and check http://hyperopt.github.io/hyperopt/getting-started/search_spaces/!
+        For big data handling, please redifined this method!
         
-        # 2. setup algo
-        raise NotImplementedError
+        Returns
+        -------
+        spaces : list
+            List of parameter expressions.
+
+        """
+        hp_tupel            = self._model._hyperparam_tupel
+        if not self._ids:
+            self._ids       = hp_tupel.get_dim_ids()
+        spaces              = []
+        for i in range(len(self._ids)):
+            hp_id           = self._ids[i]
+            hp_set          = hp_tupel.get_related_set().get_dim(hp_id)
+            hp_base_set     = hp_set._base_set
+            hp_boundaries   = hp_set.get_boundaries()
+            hp_name_short   = hp_set.get_name_short()
+            if not hp_boundaries:
+                raise NameError('Missing boundaries of a hyperparameter!')
+            else:
+                hp_low      = hp_boundaries[0]
+                hp_high     = hp_boundaries[1]
+                if hp_boundaries == Dimension.C_BASE_SET_N or hp_boundaries == Dimension.C_BASE_SET_Z:
+                    spaces.append(hp.quniform(hp_name_short,hp_low,hp_high,1))
+                elif hp_boundaries == Dimension.C_BASE_SET_Z:
+                    spaces.append(hp.uniform(hp_name_short,hp_low,hp_high))
+                else:
+                    raise NotImplementedError
+        return spaces
