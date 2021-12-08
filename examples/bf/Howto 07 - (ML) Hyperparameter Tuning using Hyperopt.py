a00b3ec1510b12a3ffbc1be45cdb76ee9de7b8c2
steveyuwono <steveyuwono@gmail.com>
2021-12-08 07:29:17 +0100

    draft howto 07 hyperopt utilization

---
+++ examples/bf/Howto 07 - (ML) Hyperparameter Tuning using Hyperopt.py
@@ -0,0 +1,28 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro
+## -- Module  : Howto 07 - (ML) Hyperparameter Tuning using Hyperopt
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2021-12-08  0.0.0     SY       Creation
+## -- 2021-12-08  1.0.0     SY       Release of first version
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 1.0.0 (2021-12-08)
+
+This module demonstrates how to utilize wrapper class for Hyperopt.
+"""
+
+
+from mlpro.wrappers.hyperopt import *
+
+
+## 1. Create a random policy and instantiate hyperparameters
+
+## 2. Prepare a scenario
+
+## 3. Customize hyperparameter setup spaces, if necessary
+
+## 4. Run the hyperparameter tuning
