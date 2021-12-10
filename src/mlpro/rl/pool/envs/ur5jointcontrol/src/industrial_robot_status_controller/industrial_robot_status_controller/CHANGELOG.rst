5433cfafb648331ac213d3347a80bec7f7806905
William Budiatmadjaja <william.budiatmadjaja@gmail.com>
2021-12-10 15:04:19 +0100

    Initial Commit #207

---
+++ src/mlpro/rl/pool/envs/ur5jointcontrol/src/industrial_robot_status_controller/industrial_robot_status_controller/CHANGELOG.rst
@@ -0,0 +1,16 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package industrial_robot_status_controller
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+0.1.2 (2019-05-12)
+------------------
+* Fix potential build ordering problem with industrial_msgs pkg. (`#4 <https://github.com/gavanderhoorn/industrial_robot_status_controller/issues/4>`_)
+* Contributors: Bianca Homberg
+
+0.1.1 (2019-04-05)
+------------------
+* Don't export build deps. (`#3 <https://github.com/gavanderhoorn/industrial_robot_status_controller/issues/3>`_)
+
+0.1.0 (2019-03-27)
+------------------
+* First release
---
+++ src/mlpro/rl/pool/envs/ur5jointcontrol/src/reinforcement_learning/openai_ros/openai_ros/src/openai_ros/openai_ros_common.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+import gym
+from .task_envs.task_envs_list import RegisterOpenAI_Ros_Env
+import roslaunch
+import rospy
+import rospkg
+import os
+import sys
+import subprocess
+
+
+def StartOpenAI_ROS_Environment(task_and_robot_environment_name, max_episode_steps=10000):
+    """
+    It Does all the stuff that the user would have to do to make it simpler
+    for the user.
+    This means:
+    0) Registers the TaskEnvironment wanted, if it exists in the Task_Envs.
+    2) Checks that the workspace of the user has all that is needed for launching this.
+    Which means that it will check that the robot spawn launch is there and the worls spawn is there.
+    4) Launches the world launch and the robot spawn.
+    5) It will import the Gym Env and Make it.
+    """
+    rospy.logwarn("Env: {} will be imported".format(
+        task_and_robot_environment_name))
+    result = RegisterOpenAI_Ros_Env(task_env=task_and_robot_environment_name,
+                                    max_episode_steps=max_episode_steps)
+
+    if result:
+        rospy.logwarn("Register of Task Env went OK, lets make the env..."+str(task_and_robot_environment_name))
+        env = gym.make(task_and_robot_environment_name)
+    else:
+        rospy.logwarn("Something Went wrong in the register")
+        env = None
+
+    return env
+
+
+class ROSLauncher(object):
+    def __init__(self, rospackage_name, launch_file_name, ros_ws_abspath="/home/user/simulation_ws"):
+
+        self._rospackage_name = rospackage_name
+        self._launch_file_name = launch_file_name
+
+        self.rospack = rospkg.RosPack()
+
+        # Check Package Exists
+        try:
+            pkg_path = self.rospack.get_path(rospackage_name)
+            rospy.logdebug("Package FOUND...")
+        except rospkg.common.ResourceNotFound:
+            rospy.logwarn("Package NOT FOUND...")
+
+        # Now we check that the Package path is inside the ros_ws_abspath
+        # This is to force the system to have the packages in that ws, and not in another.
+        if ros_ws_abspath in pkg_path:
+            rospy.logdebug("Package FOUND in the correct WS!")
+        else:
+            rospy.logwarn("Package FOUND in "+pkg_path +
+                          ", BUT not in the ws="+ros_ws_abspath+"...")
+
+        # If the package was found then we launch
+        if pkg_path:
+            rospy.loginfo(
+                ">>>>>>>>>>Package found in workspace-->"+str(pkg_path))
+            launch_dir = os.path.join(pkg_path, "launch")
+            path_launch_file_name = os.path.join(launch_dir, launch_file_name)
+
+            rospy.logwarn("path_launch_file_name=="+str(path_launch_file_name))
+            """
+            source_env_command = "source "+ros_ws_abspath+"/devel/setup.bash;"
+            roslaunch_command = "roslaunch  {0} {1}".format(rospackage_name, launch_file_name)
+            command = source_env_command+roslaunch_command
+            rospy.logwarn("Launching command="+str(command))
+
+            p = subprocess.Popen(command, shell=True)
+
+            state = p.poll()
+            if state is None:
+                rospy.loginfo("process is running fine")
+            elif state < 0:
+                rospy.loginfo("Process terminated with error")
+            elif state > 0:
+                rospy.loginfo("Process terminated without error")
+            """
+            self.uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(self.uuid)
+            self.launch = roslaunch.parent.ROSLaunchParent(
+                self.uuid, [path_launch_file_name])
+            self.launch.start()
+            
+
+
+            rospy.loginfo(">>>>>>>>>STARTED Roslaunch-->" +
+                          str(self._launch_file_name))
+        else:
+            assert False, "No Package Path was found for ROS apckage ==>" + \
+                str(rospackage_name)
