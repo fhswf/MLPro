9aaa7f32a31074f3607d1798affd9c835ecea8ef
Mochammad Rizky Diprasetya <diprasetya.rizky@pm.me>
2021-12-13 07:35:02 +0100

    Remove Plotting

--- examples/rl/Howto 15 - (RL) Train Robothtm with SB3 Wrapper.py
+++ examples/rl/Howto 15 - (RL) Train Robothtm with SB3 Wrapper.py
@@ -107,52 +107,52 @@ training.run()
 
 
 
-# 4 Create Plotting Class
-class MyDataPlotting(DataPlotting):
-    def get_plots(self):
-        """
-        A function to plot data
-        """
-        for name in self.data.names:
-            maxval  = 0
-            minval  = 0
-            if self.printing[name][0]:
-                fig     = plt.figure(figsize=(7,7))
-                raw   = []
-                label   = []
-                ax = fig.subplots(1,1)
-                ax.set_title(name)
-                ax.grid(True, which="both", axis="both")
-                for fr_id in self.data.frame_id[name]:
-                    raw.append(np.sum(self.data.get_values(name,fr_id)))
-                    if self.printing[name][1] == -1:
-                        maxval = max(raw)
-                        minval = min(raw)
-                    else:
-                        maxval = self.printing[name][2]
-                        minval = self.printing[name][1]
+# # 4 Create Plotting Class
+# class MyDataPlotting(DataPlotting):
+#     def get_plots(self):
+#         """
+#         A function to plot data
+#         """
+#         for name in self.data.names:
+#             maxval  = 0
+#             minval  = 0
+#             if self.printing[name][0]:
+#                 fig     = plt.figure(figsize=(7,7))
+#                 raw   = []
+#                 label   = []
+#                 ax = fig.subplots(1,1)
+#                 ax.set_title(name)
+#                 ax.grid(True, which="both", axis="both")
+#                 for fr_id in self.data.frame_id[name]:
+#                     raw.append(np.sum(self.data.get_values(name,fr_id)))
+#                     if self.printing[name][1] == -1:
+#                         maxval = max(raw)
+#                         minval = min(raw)
+#                     else:
+#                         maxval = self.printing[name][2]
+#                         minval = self.printing[name][1]
                     
-                    label.append("%s"%fr_id)
-                ax.plot(raw)
-                ax.set_ylim(minval-(abs(minval)*0.1), maxval+(abs(maxval)*0.1))
-                ax.set_xlabel("Episode")
-                ax.legend(label, bbox_to_anchor = (1,0.5), loc = "center left")
-                self.plots[0].append(name)
-                self.plots[1].append(ax)
-                if self.showing:
-                    plt.show()
-                else:
-                    plt.close(fig)
+#                     label.append("%s"%fr_id)
+#                 ax.plot(raw)
+#                 ax.set_ylim(minval-(abs(minval)*0.1), maxval+(abs(maxval)*0.1))
+#                 ax.set_xlabel("Episode")
+#                 ax.legend(label, bbox_to_anchor = (1,0.5), loc = "center left")
+#                 self.plots[0].append(name)
+#                 self.plots[1].append(ax)
+#                 if self.showing:
+#                     plt.show()
+#                 else:
+#                     plt.close(fig)
 
 
-# 5 Plotting 1 MLpro    
-data_printing   = {"Cycle":        [False],
-                    "Day":          [False],
-                    "Second":       [False],
-                    "Microsecond":  [False],
-                    "Smith":        [True,-1]}
+# # 5 Plotting 1 MLpro    
+# data_printing   = {"Cycle":        [False],
+#                     "Day":          [False],
+#                     "Second":       [False],
+#                     "Microsecond":  [False],
+#                     "Smith":        [True,-1]}
 
 
-mem = training.get_results().ds_rewards
-mem_plot    = MyDataPlotting(mem, p_showing=plotting, p_printing=data_printing)
-mem_plot.get_plots()
+# mem = training.get_results().ds_rewards
+# mem_plot    = MyDataPlotting(mem, p_showing=plotting, p_printing=data_printing)
+# mem_plot.get_plots()
