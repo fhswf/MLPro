0b31119b97a6098fb478bdbe4185ab0b24af5067
William Budiatmadjaja <william.budiatmadjaja@gmail.com>
2022-01-26 13:36:33 +0100

    update double pendulum env

--- src/mlpro/rl/pool/envs/doublependulum.py
+++ src/mlpro/rl/pool/envs/doublependulum.py
@@ -6,10 +6,11 @@
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
 ## -- 2022-01-25  0.0.0     WB       Creation
+## -- 2022-01-26  0.9.0     WB       Initial trial of the environment
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 0.0.0 (2022-01-25)
+Ver. 0.9.0 (2022-01-26)
 
 This module provides an RL environment of double pendulum.
 """
@@ -87,14 +88,16 @@ class DoublePendulum (Environment):
 ## -------------------------------------------------------------------------------------------------
     def __init__(self, p_logging=Log.C_LOG_ALL, t_step=0.01, t_act=20, max_torque=20,
                 max_speed=10, l1=0.5, l2=0.5, m1=0.5, m2=0.5, th1=0.0, th2=0.0, 
-                th1dot=0.0, th2dot=0.0, g=9.8, history_length=50)
-        super().__init__(p_mode=Environment.C_MODE_SIM, p_logging=p_logging)
+                th1dot=0.0, th2dot=0.0, g=9.8, history_length=50):
         self.t_step = t_step
         self.t_act = t_act
+        self.set_latency(timedelta(0,t_act*t_step,0))
+        
         self.max_torque = max_torque
         self.max_speed = max_speed
         self.l1 = l1
         self.l2 = l2
+        self.L = l1+l2
         self.m1 = m1
         self.m2 = m2
         self.th1 = th1
@@ -102,14 +105,18 @@ class DoublePendulum (Environment):
         self.th1dot = th1dot
         self.th2dot = th2dot
         self.g = g
-        self.history_length=50
+        
+        self.history_x = deque(maxlen=history_length)
+        self.history_y = deque(maxlen=history_length)
+        
+        super().__init__(p_mode=Environment.C_MODE_SIM, p_logging=p_logging)
+        self._state = State(self._state_space)
         
         self.reset()  
 
 
 ## -------------------------------------------------------------------------------------------------
-    @staticmethod
-    def setup_spaces():
+    def setup_spaces(self):
         """
         This method is used to setup action and state spaces of the system.
 
@@ -123,13 +130,199 @@ class DoublePendulum (Environment):
         """
         state_space     = ESpace()
         action_space    = ESpace()
-
-        state_space.add_dim(Dimension(0, 'theta 1', 'th1', 'Angle of Pendulum 1', '', 'degrees', '\textdegrees',[-np.pi, np.pi]))
-        state_space.add_dim(Dimension(0, 'omega 1', 'w1', 'Angular Velocity of Pendulum 1', '', 'degrees/second', '\textdegrees/s',[]))
-        state_space.add_dim(Dimension(0, 'theta 2', 'th2', 'Angle of pendulum 2', '', 'degrees', '\textdegrees',[-np.pi, np.pi]))
-        state_space.add_dim(Dimension(0, 'omega 2', 'w2', 'Angular Velocity of Pendulum 2', '', 'degrees/second', '\textdegrees/s',[]))
         
-        action_space.add_dim(Dimension(0, 'E-0 Act', 'R', 'Env-0 Actuator Control', '', '', '', []))
+        state_space.add_dim(Dimension(0, 'theta 1', 'th1', 'Angle of Pendulum 1', '', 'degrees', 
+                            '\textdegrees',[-np.pi, np.pi]))
+        state_space.add_dim(Dimension(1, 'omega 1', 'w1', 'Angular Velocity of Pendulum 1', '', 
+                            'degrees/second', '\textdegrees/s',[]))
+        state_space.add_dim(Dimension(2, 'theta 2', 'th2', 'Angle of pendulum 2', '', 'degrees', 
+                            '\textdegrees',[-np.pi, np.pi]))
+        state_space.add_dim(Dimension(3, 'omega 2', 'w2', 'Angular Velocity of Pendulum 2', '', 
+                            'degrees/second', '\textdegrees/s',[]))
+        
+        action_space.add_dim(Dimension(0, 'torque 1', 'tau1', 'Applied Torque of Motor 1', '', 
+                            'Nm', 'Nm', [-self.max_torque, self.max_torque]))
 
         return state_space, action_space
+        
 
+## -------------------------------------------------------------------------------------------------
+    @staticmethod
+    def derivs(state, t):
+        dydx = np.zeros_like(state)
+        dydx[0] = state[1]
+
+        delta = state[2] - state[0]
+        den1 = (M1+M2) * L1 - M2 * L1 * cos(delta) * cos(delta)
+        dydx[1] = ((M2 * L1 * state[1] * state[1] * sin(delta) * cos(delta)
+                    + M2 * G * sin(state[2]) * cos(delta)
+                    + M2 * L2 * state[3] * state[3] * sin(delta)
+                    - (M1+M2) * G * sin(state[0]))
+                   / den1)
+
+        dydx[2] = state[3]
+
+        den2 = (L2/L1) * den1
+        dydx[3] = ((- M2 * L2 * state[3] * state[3] * sin(delta) * cos(delta)
+                    + (M1+M2) * G * sin(state[0]) * cos(delta)
+                    - (M1+M2) * L1 * state[1] * state[1] * sin(delta)
+                    - (M1+M2) * G * sin(state[2]))
+                   / den2)
+
+        return dydx
+        
+
+## -------------------------------------------------------------------------------------------------
+    @staticmethod
+    def angle_normalize(x):
+        return ((x + np.pi) % (2 * np.pi)) - np.pi
+        
+
+## -------------------------------------------------------------------------------------------------
+    def _reset(self, p_seed=None) -> None:
+        """
+        This method is used to reset the environment.
+
+        Parameters
+        ----------
+        p_seed : int, optional
+            Not yet implemented. The default is None.
+
+        """
+        self._state.set_value(0, np.radians(self.th1))
+        self._state.set_value(1, np.radians(self.th1dot))
+        self._state.set_value(2, np.radians(self.th2))
+        self._state.set_value(3, np.radians(self.th2dot))
+        
+        self.history_x.clear()
+        self.history_y.clear()
+
+
+## -------------------------------------------------------------------------------------------------
+    def _simulate_reaction(self, p_state:State, p_action:Action) -> State:
+        """
+        This method is used to calculate the next states of the system after a set of actions.
+
+        Parameters
+        ----------
+        p_state : State
+            State.
+        p_action : Action
+            ACtion.
+
+        Returns
+        -------
+        _state : State
+            current states.
+
+        """
+        state = p_state.get_values()
+        th1, th1dot, th2, th2dot = state
+        torque = p_action.get_sorted_values()[0]
+        torque = np.clip(torque, -self.max_torque, self.max_torque)
+        
+        state[1] = th1dot + (3 * self.g / (2 * self.l1) * sin(th1) + 3.0 / 
+                (self.m1 * self.l1 ** 2) * torque) * self.t_step
+                
+        if abs(th1dot) > max_speed:
+            state[1] = np.clip(state[1], -th1dot, th1dot)
+        else:
+            state[1] = np.clip(state[1], -self.max_speed, self.max_speed)
+        
+        self.y = integrate.odeint(derivs, state, np.arange(0, self.t_act*self.t_step, self.t_step))
+        state = self.y[-1]
+        
+        for i in range(len(state)):
+            self._state.set_value(i, state[i])
+
+        return self._state
+
+
+## -------------------------------------------------------------------------------------------------
+    def _compute_broken(self, p_state:State) -> bool:
+        """
+        This method computes the broken flag. This method can be redefined.
+
+        Parameters
+        ----------
+        p_state : State
+            state.
+
+        Returns
+        -------
+        bool
+            broken or not.
+
+        """ 
+
+        return False
+
+
+## -------------------------------------------------------------------------------------------------
+    def _compute_success(self, p_state:State) -> bool:
+        """
+        This method computes the success flag. This method can be redefined.
+
+        Parameters
+        ----------
+        p_state : State
+            state.
+
+        Returns
+        -------
+        bool
+            success or not success.
+
+        """
+        
+        return False
+    
+
+## -------------------------------------------------------------------------------------------------
+    def _compute_reward(self, p_state_old:State, p_state_new:State) -> Reward:
+        reward = Reward(Reward.C_TYPE_OVERALL)
+        count = 0
+        for th1 in self.y[:,0]:
+            if np.degrees(th1) > 179 or np.degrees(th1) < 181 or \
+                np.degrees(th1) < -179 or np.degrees(th1) > -181:
+                count += 1
+        
+        reward.set_overall_reward(count/len(self.y))
+        
+        return reward
+    
+
+## -------------------------------------------------------------------------------------------------
+    def init_plot(self, p_figure=None):
+        self.fig = plt.figure(figsize=(5,4)) if p_figure==None else p_figure
+        self.ax = self.fig.add_subplot(autoscale_on=False, 
+                    xlim=(-self.L*1.2, self.L*1.2), ylim=(-self.L*1.2, self.L*1.2))
+        self.ax.set_aspect('equal')
+        self.ax.grid()
+        
+        self.line, = self.ax.plot([], [], 'o-', lw=2)
+        self.trace, = self.ax.plot([], [], '.-', lw=1, ms=2)
+
+
+## -------------------------------------------------------------------------------------------------
+    def update_plot():
+        x1 = self.l1*sin(self.y[:, 0])
+        y1 = -self.l1*cos(self.y[:, 0])
+
+        x2 = self.l2*sin(self.y[:, 2]) + x1
+        y2 = -self.l2*cos(self.y[:, 2]) + y1
+
+        def animate(i):
+            thisx = [0, x1[i], x2[i]]
+            thisy = [0, y1[i], y2[i]]
+
+            self.history_x.appendleft(thisx[2])
+            self.history_y.appendleft(thisy[2])
+
+            self.line.set_data(thisx, thisy)
+            self.trace.set_data(self.history_x, self.history_y)
+            return line, trace
+
+        ani = animation.FuncAnimation(
+            self.fig, animate, len(self.y), interval=self.t_step*1000, blit=True, repeat=False)
+        plt.show()
