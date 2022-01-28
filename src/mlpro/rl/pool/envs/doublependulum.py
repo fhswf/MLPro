0ffad3e138e5ca94a62651199655902207b876bd
William Budiatmadjaja <william.budiatmadjaja@gmail.com>
2022-01-28 10:59:22 +0100

    Fix update_plot method in the double pendulum env

--- src/mlpro/rl/pool/envs/doublependulum.py
+++ src/mlpro/rl/pool/envs/doublependulum.py
@@ -8,10 +8,11 @@
 ## -- 2022-01-25  0.0.0     WB       Creation
 ## -- 2022-01-26  0.9.0     WB       Initial trial of the environment
 ## -- 2022-01-27  0.9.1     WB       Trial without animation 
+## -- 2022-01-28  0.9.2     WB       Fix the  update_plot method
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 0.9.0 (2022-01-26)
+Ver. 0.9.2 (2022-01-28)
 
 This module provides an RL environment of double pendulum.
 """
@@ -67,7 +68,7 @@ class DoublePendulum (Environment):
     g : float, optional
         gravitational acceleration. The default is 9.8
     history_length : int, optional
-        historical trajectory points to display. The default is 20.
+        historical trajectory points to display. The default is 2.
         
     Attributes
     ----------
@@ -89,7 +90,7 @@ class DoublePendulum (Environment):
 ## -------------------------------------------------------------------------------------------------
     def __init__(self, p_logging=Log.C_LOG_ALL, t_step=0.01, t_act=20, max_torque=20,
                 max_speed=10, l1=0.5, l2=0.5, m1=0.5, m2=0.5, th1=0.0, th2=0.0, 
-                th1dot=0.0, th2dot=0.0, g=9.8, history_length=20):
+                th1dot=0.0, th2dot=0.0, g=9.8, history_length=2):
         self.t_step = t_step
         self.t_act = t_act
         self.set_latency(timedelta(0,t_act*t_step,0))
@@ -296,7 +297,14 @@ class DoublePendulum (Environment):
     def init_plot(self, p_figure=None):
         if hasattr(self, 'fig'):
             plt.close(self.fig)
-        self.fig = plt.figure(figsize=(5,4)) if p_figure==None else p_figure
+        
+        if p_figure==None:
+            self.fig = plt.figure(figsize=(5,4)) 
+            self.embedded_fig = False
+        else:
+            self.fig = p_figure
+            self.embedded_fig = True
+            
         self.ax = self.fig.add_subplot(autoscale_on=False, 
                     xlim=(-self.L*1.2, self.L*1.2), ylim=(-self.L*1.2, self.L*1.2))
         self.ax.set_aspect('equal')
@@ -313,20 +321,6 @@ class DoublePendulum (Environment):
 
         x2 = self.l2*sin(self.y[:, 2]) + x1
         y2 = -self.l2*cos(self.y[:, 2]) + y1
-        
-        # def animate(i):
-            # thisx = [0, x1[i], x2[i]]
-            # thisy = [0, y1[i], y2[i]]
-
-            # self.history_x.appendleft(thisx[2])
-            # self.history_y.appendleft(thisy[2])
-
-            # self.line.set_data(thisx, thisy)
-            # self.trace.set_data(self.history_x, self.history_y)
-            # return self.line, self.trace
-
-        # ani = animation.FuncAnimation(
-            # self.fig, animate, len(self.y), interval=self.t_step*1000, blit=True, repeat=False)
 
         thisx = [0, x1[-1], x2[-1]]
         thisy = [0, y1[-1], y2[-1]]            
@@ -335,4 +329,6 @@ class DoublePendulum (Environment):
         self.line.set_data(thisx, thisy)
         self.trace.set_data(self.history_x, self.history_y)
         
-        plt.show()
+        if not self.embedded_fig:
+            self.fig.canvas.draw()
+            self.fig.canvas.flush_events()
