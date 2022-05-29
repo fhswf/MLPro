0238837656beaadcfc2d13eb64fc12309a30c4be
detlefarend <detlef@arend-privat.net>
2022-05-29 14:24:04 +0200

    Update unit test for howtos

--- test/test_examples.py
+++ test/test_examples.py
@@ -11,10 +11,11 @@
 ## -- 2021-12-20  1.0.2     DA       Howto 08 disabled
 ## -- 2022-02-28  1.0.3     SY       Howto 06, 07 of basic functions are added
 ## -- 2022-02-28  1.0.4     SY       Howto 07 of basic functions is disabled
+## -- 2022-05-29  1.1.0     DA       Update howto list after refactoring of all howto files
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 1.0.4 (2022-02-28)
+Ver. 1.1.0 (2022-05-29)
 
 Unit test for all examples available.
 """
@@ -30,21 +31,12 @@ howto_list = {
     "bf_004": "mlpro.bf.examples.howto_bf_004_store_plot_and_save_variables",
     "bf_005": "mlpro.bf.examples.howto_bf_005_hyperparameters",
     "bf_006": "mlpro.bf.examples.howto_bf_006_buffers",
-    "bf_007": "mlpro.bf.examples.howto_bf_007_hyperparameter_tuning_using_hyperopt",
-    "bf_008": "mlpro.bf.examples.howto_bf_008_hyperparameter_tuning_using_optuna",
+#    "bf_007": "mlpro.bf.examples.howto_bf_007_hyperparameter_tuning_using_hyperopt",
+#    "bf_008": "mlpro.bf.examples.howto_bf_008_hyperparameter_tuning_using_optuna",
     "bf_009": "mlpro.bf.examples.howto_bf_009_sciui_reuse_of_interactive_2d_3d_input_space",
     "bf_010": "mlpro.bf.examples.howto_bf_010_sciui_reinforcement_learning_cockpit"
 }
 
-
-# import mlpro.bf.examples
-# from pkgutil import iter_modules
-
-# def list_submodules(module):
-#     for submodule in iter_modules(module.__path__):
-#         print(submodule.name)
-
-# list_submodules(mlpro.bf.examples)
 
 
 @pytest.mark.parametrize("cls", list(howto_list.keys()))
