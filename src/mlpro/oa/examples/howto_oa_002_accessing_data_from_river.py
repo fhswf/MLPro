a677622c4f6ea04f5ad7bfb1445154b35184550b
Laxmikant Shrikant Baheti <100450659+laxmikantbaheti@users.noreply.github.com>
2022-06-14 17:49:44 +0200

    howto for river

---
+++ src/mlpro/oa/examples/howto_oa_002_accessing_data_from_river.py
@@ -0,0 +1,58 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro
+## -- Module  : howto002_oa_accessing_data_from_river
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2022-06-14  0.0.0     LSB      Creation
+## -- 2022-06-14  1.0.0     LSB      Release of first version
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 1.0.0 (2022-06-14)
+
+This module shows how to wrap mlpro's Stream and StreamProvider class to River.
+"""
+from mlpro.wrappers.river import *
+
+
+# 1. Create a Wrapper for OpenML stream provider
+river_wrap = WrStreamProviderRiver()
+
+
+# 2. Get a list of streams available at the stream provider
+stream_list = river_wrap.get_stream_list()
+
+if __name__ == "__main__":
+    for stream in stream_list:
+        print('stream id: '+ str(stream.get_id( )) + ' stream name: ' + str(stream.get_name()))
+
+
+# 3. Get a specific stream from the stream provider
+stream = river_wrap.get_stream(2)
+
+
+# 4. get the feature space of the stream
+river_wrap.log(stream.C_LOG_TYPE_I,"Number of features in the stream:",stream.get_feature_space().get_num_dim())
+# print("Number of features in the stream:",stream.get_feature_space().get_num_dim())
+
+
+# 5. resetting the stream
+stream.reset()
+
+
+# 6. Loading stream instances
+for i in range(10):
+    curr_instance = stream.get_next().get_values()
+    stream.log(stream.C_LOG_TYPE_I,'Current Instance:' , curr_instance)
+
+
+# 7. resetting the stream
+stream.reset()
+
+
+# 8. Getting stream instances
+for i in range(5):
+    curr_instance = stream.get_next().get_values()
+    stream.log(stream.C_LOG_TYPE_I, 'Current Instance:' , curr_instance)
