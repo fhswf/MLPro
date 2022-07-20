829a915d278312aad0a295fa4b82973737104625
steveyuwono <steveyuwono@gmail.com>
2022-07-20 16:29:36 +0200

    test

--- src/mlpro/rl/examples/howto_rl_013_model_based_reinforcement_learning.py
+++ src/mlpro/rl/examples/howto_rl_013_model_based_reinforcement_learning.py
@@ -9,11 +9,11 @@
 ## -- 2021-12-17  1.0.0     MRD       Released first version
 ## -- 2022-01-01  1.0.1     MRD       Refactoring due to new model implementation
 ## -- 2022-05-20  1.0.2     MRD       Add HTMEnvModel
-## -- 2022-07-18  1.0.3     SY        Update due to introduction of ActionPlanner
+## -- 2022-07-20  1.0.3     SY        Update due to introduction of ActionPlanner
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 1.0.3 (2022-07-18)
+Ver. 1.0.3 (2022-07-20)
 
 This module demonstrates model-based reinforcement learning (MBRL) with action planner using MPC.
 """
