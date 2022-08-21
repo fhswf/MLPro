71177ab926d5227ff56bade528c6504464bb6dd9
detlefarend <detlef@arend-privat.net>
2022-08-21 12:05:53 +0200

    BF: Event handling

---
+++ src/mlpro/bf/examples/howto_bf_011_event_handling.py
@@ -0,0 +1,30 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro
+## -- Module  : howto_bf_001_logging.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2022-08-21  0.0.0     DA       Creation
+## -- 2022-08-dd  1.0.0     DA       First implementation
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 1.0.0 (2022-08-dd)
+
+This module demonstrates the use of MLPro's event handling as a property in own classes.
+"""
+
+
+
+
+
+# 1 Reuse logging property in your own class by inheriting from class Log
+class MyClass: pass
+
+
+
+
+
+if __name__ == "__main__":
+    pass
