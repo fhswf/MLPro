4fee5d843cef9f975e2a7fea0f477fa57c855b53
Laxmikant Shrikant Baheti <100450659+laxmikantbaheti@users.noreply.github.com>
2022-09-16 17:56:16 +0200

    refactored the file structure

--- src/mlpro/bf/math/normalizers.py
+++ src/mlpro/bf/math/normalizers.py
@@ -13,10 +13,9 @@ Ver. 0.0.0 (2022-09-16)
 This module provides base class for Normalizers.
 """
 
-
 from mlpro.bf.math import *
-
-
+from mlpro.rl.models_sar import *
+import numpy
 
 
 
@@ -29,54 +28,59 @@ class Normalizer:
 
 
 ## -------------------------------------------------------------------------------------------------
-    def __init__(self, p_param = None):
+    def __init__(self, p_set:Set):
 
-        self._param = p_param
+
+        self._param = None
         self._old_param = None
         self._new_param = None
+        self._set_parameters(p_set)
 
 
 ## -------------------------------------------------------------------------------------------------
     def get_parameters(self):
-
-        pass
-
-
-## -------------------------------------------------------------------------------------------------
-    def _set_parameters(self):
-
-        pass
+        return self._param
 
 
 ## -------------------------------------------------------------------------------------------------
-    def normalize(self):
+    def _set_parameters(self, p_set:Set):
 
-        pass
-
-
-## -------------------------------------------------------------------------------------------------
-    def _normalize(self):
 
         raise NotImplementedError
 
 
 ## -------------------------------------------------------------------------------------------------
-    def denormalize(self):
+    def normalize(self, p_element:Element):
 
-        pass
+        element = self._normalize(p_element)
+        return element
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _denormalize(self):
+    def _normalize(self, p_element:Element):
 
         raise NotImplementedError
 
 
 ## -------------------------------------------------------------------------------------------------
-    def renormalize(self):
+    def denormalize(self,p_element:Element):
+        element = self._denormalize(p_element)
+        return element
 
-        self.denormalize()
-        self.normalize()
+
+## -------------------------------------------------------------------------------------------------
+    def _denormalize(self, p_element:Element):
+
+        raise NotImplementedError
+
+
+## -------------------------------------------------------------------------------------------------
+    def renormalize(self, p_element:Element):
+
+        denormalized_element = self.denormalize(p_element)
+        renormalized_element = self.normalize(denormalized_element)
+        return renormalized_element
+
 
 
 ## -------------------------------------------------------------------------------------------------
@@ -97,18 +101,35 @@ class NormalizerMinMax(Normalizer):
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _normalize(self):
+    def _normalize(self, p_element:Element):
+
+        normalized_element = np.multiply(p_element.get_values(), self._param[0])+self._param[1]
+        return normalized_element
+
+
+## -------------------------------------------------------------------------------------------------
+    def _denormalize(self, p_element:Element):
 
         pass
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _denormalize(self):
+    def update_param(self, p_set:Set):
 
         pass
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _update_param(self, p_space:ESpace):
+    def _set_parameters(self, p_set:Set):
 
-        pass
+        if p_set is None: raise ValueError('Set not provided')
+        a = []
+        b = []
+        for i in p_set.get_dim_ids():
+            min_boundary = p_set.get_dim(i).get_boundaries()[0]
+            max_boundary = p_set.get_dim(i).get_boundaries()[1]
+            range = max_boundary-min_boundary
+            a.append(2/(range))
+            b.append(2*min_boundary+(1/(range)))
+
+        self._param = np.array(a,b)
