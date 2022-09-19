f587743d811058343c540c51c9cb240e3b1f0fe1
steveyuwono <steveyuwono@gmail.com>
2022-09-19 10:55:32 +0200

    draft howto MPC

---
+++ src/mlpro/rl/examples/howto_rl_026_train_mbrl_using_mpc_on_grid_world.py
@@ -0,0 +1,21 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : FH-SWF Automation Technology - Common Code Base (CCB)
+## -- Package : mlpro
+## -- Module  : howto_rl_026_train_mbrl_using_mpc_on_grid_world.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2022-09-19  0.0.0     SY       Creation
+## -- 2022-??-??  1.0.0     SY       Release first version
+## -------------------------------------------------------------------------------------------------
+
+
+"""
+Ver. 0.0.0 (2022-09-19)
+
+This module shows how to incorporate MPC in Model-Based RL on Grid World problem.
+
+Currently under construction...
+"""
+
+
