f34654df248990c16641eab4131fce1ca2450221
steveyuwono <steveyuwono@gmail.com>
2022-11-02 10:25:32 +0100

    howto_rl_wp_003 : bug fixing related to render

--- src/mlpro/rl/examples/howto_rl_wp_003_run_multiagent_with_own_policy_on_petting_zoo_environment.py
+++ src/mlpro/rl/examples/howto_rl_wp_003_run_multiagent_with_own_policy_on_petting_zoo_environment.py
@@ -23,7 +23,7 @@
 ## -- 2022-10-08  1.2.0     SY       Turn off render: causing error due to pzoo ver 1.22.0 
 ## -- 2022-10-14  1.2.1     SY       Refactoring 
 ## -- 2022-11-01  1.2.2     DA       Refactoring 
-## -- 2022-11-02  1.2.3     SY       Unable logging in unit test model 
+## -- 2022-11-02  1.2.3     SY       Unable logging in unit test model and bug fixing
 ## -------------------------------------------------------------------------------------------------
 
 """
@@ -59,7 +59,10 @@ class PBScenario (RLScenario):
     C_NAME      = 'Pistonball V6'
 
     def _setup(self, p_mode, p_ada, p_logging):
-        zoo_env             = pistonball_v6.env()
+        if self._visualize:
+            zoo_env         = pistonball_v6.env(render_mode="human")
+        else:
+            zoo_env         = pistonball_v6.env(render_mode="ansi")
         self._env           = WrEnvPZOO2MLPro(zoo_env, p_visualize=self._visualize, p_logging=p_logging)
         
         multi_agent         = MultiAgent(p_name='Pistonball_agents', p_ada=1, p_logging=p_logging)
@@ -97,7 +100,10 @@ class C4Scenario (RLScenario):
     C_NAME      = 'Connect Four V3'
 
     def _setup(self, p_mode, p_ada, p_logging):
-        zoo_env             = connect_four_v3.env()
+        if self._visualize:
+            zoo_env         = connect_four_v3.env(render_mode="ansi")
+        else:
+            zoo_env         = connect_four_v3.env(render_mode="human")
         self._env           = WrEnvPZOO2MLPro(zoo_env, p_visualize=self._visualize, p_logging=p_logging)
         
         multi_agent         = MultiAgent(p_name='Connect4_Agents', p_ada=1, p_logging=p_logging)
