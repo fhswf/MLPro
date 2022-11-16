014ec9abf1b4db70adfb5e6cdee8a2644929d576
Mochammad Rizky Diprasetya <diprasetya.rizky@protonmail.com>
2022-11-16 20:39:49 +0100

    exclude robothtm due to torch

--- test/test_environment.py
+++ test/test_environment.py
@@ -24,7 +24,7 @@ import pytest
 import random
 import numpy as np
 from mlpro.rl.models import *
-from mlpro.rl.pool.envs.robotinhtm import RobotHTM
+# from mlpro.rl.pool.envs.robotinhtm import RobotHTM
 from mlpro.rl.pool.envs.bglp import BGLP
 from mlpro.rl.pool.envs.gridworld import GridWorld
 from mlpro.rl.pool.envs.multicartpole import MultiCartPole
@@ -34,7 +34,7 @@ from mlpro.rl.pool.envs.doublependulum import DoublePendulumS4
 
 
 ## -------------------------------------------------------------------------------------------------
-@pytest.mark.parametrize("env_cls", [RobotHTM, BGLP, GridWorld, MultiCartPole, DoublePendulumS7, DoublePendulumS4])
+@pytest.mark.parametrize("env_cls", [BGLP, GridWorld, MultiCartPole, DoublePendulumS7, DoublePendulumS4])
 def test_environment(env_cls):
     env = env_cls()
     assert isinstance(env, Environment)
