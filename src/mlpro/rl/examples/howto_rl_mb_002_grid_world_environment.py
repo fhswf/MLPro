5c02fcedf8adf33bdde11f99501e989d170c062c
steveyuwono <steveyuwono@gmail.com>
2023-03-08 09:43:19 +0100

    refactoring howto mbrl 02

--- src/mlpro/rl/examples/howto_rl_mb_002_grid_world_environment.py
+++ src/mlpro/rl/examples/howto_rl_mb_002_grid_world_environment.py
@@ -79,8 +79,8 @@ class GridWorldAFct(PyTorchMLP):
         ids_ = self._hyperparam_tuple.get_dim_ids()
         self._hyperparam_tuple.set_value(ids_[0], self._input_space.get_num_dim())
         self._hyperparam_tuple.set_value(ids_[1], self._output_space.get_num_dim())
-        self._hyperparam_tuple.set_value(ids_[2], p_par['p_input_size'])
-        self._hyperparam_tuple.set_value(ids_[3], p_par['p_output_size'])
+        self._hyperparam_tuple.set_value(ids_[2], p_par['p_update_rate'])
+        self._hyperparam_tuple.set_value(ids_[3], p_par['p_num_hidden_layers'])
         self._hyperparam_tuple.set_value(ids_[4], p_par['p_hidden_size'])
         self._hyperparam_tuple.set_value(ids_[5], p_par['p_act_fct'])
         self._hyperparam_tuple.set_value(ids_[6], p_par['p_output_afc_fct'])
@@ -117,8 +117,8 @@ class MLPEnvModel(EnvModel):
             p_buffer_size=5000,
             p_ada=p_ada,
             p_logging=p_logging,
-            p_input_size=1,
-            p_output_size=3,
+            p_update_rate=1,
+            p_num_hidden_layers=3,
             p_hidden_size=128,
             p_act_fct=torch.nn.ReLU,
             p_output_afc_fct=torch.nn.ReLU,
