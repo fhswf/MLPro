513cce16aa091078c1986f5ac1888310e269fb77
detlefarend <detlef@arend-privat.net>
2023-03-20 12:16:02 +0100

    set version 1.0.1

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlpro',
-version='1.0.0',
+version='1.0.1',
 description='MLPro - The Integrative Middleware Framework for Standardized Machine Learning',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
