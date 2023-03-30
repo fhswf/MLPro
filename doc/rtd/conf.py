fda26520acb5e825c9768c748550a3cdfa2aeb15
detlefarend <detlef@arend-privat.net>
2023-03-30 10:45:56 +0200

    set to version 1.1.0

--- doc/rtd/conf.py
+++ doc/rtd/conf.py
@@ -23,7 +23,7 @@ copyright = "2023 South Westphalia University of Applied Sciences, Germany"
 author = "Detlef Arend, Steve Yuwono, Mochammad Rizky Diprasetya et al"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.2"
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------
