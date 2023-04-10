d1162f302e7c5b4409617806402ada1448529a70
detlefarend <detlef@arend-privat.net>
2023-04-10 13:12:39 +0200

    OA-Task: Cluster Analyzer #589

--- src/mlpro/oa/streams/tasks/clusteranalyzers.py
+++ src/mlpro/oa/streams/tasks/clusteranalyzers.py
@@ -1,23 +1,22 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
-## -- Package : mlpro.oa.tasks.boundarydetectors
-## -- Module  : anomalydetectors.py
+## -- Package : mlpro.oa.tasks
+## -- Module  : clusteranalyzers.py
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
 ## -- 2023-01-24  0.0.0     DA       Creation
+## -- 2023-04-10  0.1.0     DA       First implementation of classes ClusterMembership, ClusterAnalyzer
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 0.0.0 (2023-01-24)
+Ver. 0.1.0 (2023-04-10)
 
 This module provides templates for cluster analysis to be used in the context of online adaptivity.
 """
 
 from mlpro.oa.streams import *
 import random
-
-
 
 
 
@@ -213,13 +212,54 @@ class Cluster (Model):
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
+class ClusterMembership:
+    """
+    Defnes the membership of an instance to a cluster.
+
+    Parameters
+    ----------
+    p_cluster : Cluster
+        Related cluster object.
+    p_membership : float
+        Relative membership of an instance to the related cluster in percent.
+    """
+
+## -------------------------------------------------------------------------------------------------
+    def __init__(self, p_cluster : Cluster, p_membership : float ):
+        
+        self.cluster = p_cluster 
+        self.membership = p_membership
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 class ClusterAnalyzer (OATask):
     """
     This is the base class for multivariate online cluster analysis. It raises an event when a cluster
     was added or removed.
+
+    Parameters
+    ----------
+    p_name : str
+        Optional name of the task. Default is None.
+    p_range_max : int
+        Maximum range of asynchonicity. See class Range. Default is Range.C_RANGE_PROCESS.
+    p_ada : bool
+        Boolean switch for adaptivitiy. Default = True.
+    p_duplicate_data : bool
+        If True, instances will be duplicated before processing. Default = False.
+    p_visualize : bool
+        Boolean switch for visualisation. Default = False.
+    p_logging
+        Log level (see constants of class Log). Default: Log.C_LOG_ALL
+    p_kwargs : dict
+        Further optional named parameters.
     """
 
-    C_NAME                  = 'Cluster Analyzer'
+    C_TYPE                  = 'Cluster Analyzer'
 
     C_EVENT_CLUSTER_ADDED   = 'CLUSTER_ADDED'
     C_EVENT_CLUSTER_REMOVED = 'CLUSTER_REMOVED'
@@ -227,16 +267,60 @@ class ClusterAnalyzer (OATask):
     C_PLOT_ACTIVE           = True
     C_PLOT_STANDALONE       = False
 
+## -------------------------------------------------------------------------------------------------
     def __init__( self, 
-                p_name: str = None, p_range_max=StreamTask.C_RANGE_THREAD, p_ada: bool = True, p_duplicate_data: bool = False, p_visualize: bool = False, p_logging=Log.C_LOG_ALL, **p_kwargs):
-        super().__init__(p_name, p_range_max, p_ada, p_duplicate_data, p_visualize, p_logging, **p_kwargs)
+                  p_name : str = None, 
+                  p_range_max = StreamTask.C_RANGE_THREAD, 
+                  p_ada : bool = True, 
+                  p_duplicate_data : bool = False, 
+                  p_visualize : bool = False, 
+                  p_logging = Log.C_LOG_ALL, 
+                  **p_kwargs ):
+        
+        self._clusters = []
+        
+        super().__init__( p_name=p_name, 
+                          p_range_max=p_range_max, 
+                          p_ada=p_ada, 
+                          p_duplicate_data=p_duplicate_data, 
+                          p_visualize=p_visualize, 
+                          p_logging=p_logging, 
+                          **p_kwargs )
 
 
 ## -------------------------------------------------------------------------------------------------
-    def set_random_seed(self, p_seed=None):
-        return super().set_random_seed(p_seed)
+    def _run(self, p_inst_new: list[Instance], p_inst_del: list[Instance]):
+        self.adapt( p_inst_new=p_inst_new, p_inst_del=p_inst_del )
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _run(self, p_inst_new: list, p_inst_del: list):
+    def get_clusters(self) -> list[Cluster]:
+        """
+        Returns the current list of clusters.
+
+        Returns
+        -------
+        list_of_clusters : list[Cluster]
+            Current list of clusters.
+        """
+
+        return self._clusters
+    
+
+## -------------------------------------------------------------------------------------------------
+    def get_cluster_membership( p_inst : Instance ) -> list[ClusterMembership]:
+        """
+        Public custom method to determine the membership of the given instance to each cluster as
+        a value in percent.
+
+        Parameters
+        ----------
+        p_inst : Instance
+            Instance to be evaluated.
+
+        Returns
+        membership : list
+            List of per
+        """
+
         raise NotImplementedError
