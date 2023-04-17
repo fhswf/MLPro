bcd9e52e62d9b3c3b42e774b4902595f487e286a
steveyuwono <steveyuwono@gmail.com>
2023-04-17 11:24:35 +0200

    howto_bf_streams_005_sampler #643

--- doc/rtd/content/04_appendices/appendix1/sub/mlpro_bf/layer3_application_support/streams/howto.bf.streams.005.rst
+++ doc/rtd/content/04_appendices/appendix1/sub/mlpro_bf/layer3_application_support/streams/howto.bf.streams.005.rst
@@ -35,6 +35,24 @@ Howto BF-STREAMS-005: Streams Sampler
     YYYY-MM-DD  HH:MM:SS.SSSSSS  I  Stream CSV File "data_storage.csv": Reset
     YYYY-MM-DD  HH:MM:SS.SSSSSS  W  Stream CSV File "data_storage.csv": Number of instances being sampled: 5000
 
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  I  Stream "Random 10D x 1000": Instantiated 
+
+    Press ENTER to iterate all streams dark...
+
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  I  Stream "Random 10D x 1000": Reset
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  W  Stream "Random 10D x 1000": Features: 10 , Labels: 2 , Instances: 1000
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  I  Stream "Random 10D x 1000": Reset
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  W  Stream "Random 10D x 1000": Number of instances being sampled: 150
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  W  Stream "Random 10D x 1000": Done in 0.008  seconds (throughput = 133174 instances/sec)
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  I  Stream "Random 10D x 1000": Reset
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  W  Stream "Random 10D x 1000": Number of instances being sampled: 239
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  I  Stream "Random 10D x 1000": Reset
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  W  Stream "Random 10D x 1000": Number of instances being sampled: 100 
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  I  Stream "Random 10D x 1000": Reset
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  W  Stream "Random 10D x 1000": Number of instances being sampled: 333
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  I  Stream "Random 10D x 1000": Reset
+    YYYY-MM-DD  HH:MM:SS.SSSSSS  W  Stream "Random 10D x 1000": Number of instances being sampled: 50
+
 
 **Cross Reference**
 
--- src/mlpro/bf/examples/howto_bf_streams_005_sampler.py
+++ src/mlpro/bf/examples/howto_bf_streams_005_sampler.py
@@ -8,25 +8,23 @@
 ## -- 2023-04-10  0.0.0     SY       Creation 
 ## -- 2023-04-14  1.0.0     SY       First release
 ## -- 2023-04-16  1.0.1     SY       Add more sampler methods to this howto
+## -- 2023-04-17  1.1.0     SY       Replace StreamMLProCSV to Rnd10Dx1000
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 1.0.1 (2023-04-16)
+Ver. 1.0.1 (2023-04-17)
 
-This howto file demonstrates the incorporation of a stream sampler in a stream scenario with loading
-and converting data stored in csv files to be compatible for data streams provided by MLPro.
+This howto file demonstrates the incorporation of a stream sampler in a stream scenario.
 
 You will learn:
 
-1) How to load and convert data from csv files.
+1) How to access a MLPro's native data stream.
 
 2) How to iterate the instances of a native stream.
-
-3) How to access feature data of a native stream.
     
-4) How to incorporate a stream sampler.
+3) How to incorporate a stream sampler.
 
-5) Update a sampler method with another sampler method after instantiation of a stream
+4) Update a sampler method with another sampler method after instantiation of a stream
 
 """
 
@@ -36,119 +34,82 @@ from mlpro.bf.streams.streams import *
 from mlpro.bf.streams.samplers import *
 from mlpro.bf.various import *
 from mlpro.bf.data import *
-import random
-from pathlib import Path
 
 
 
 # 0 Prepare Demo/Unit test mode
 if __name__ == '__main__':
     logging     = Log.C_LOG_ALL
-    
-    
-    # 1 Generate random data and store them in csv format
-    num_eps         = 10
-    num_cycles      = 10000
-    data_names      = ["action","states_1","states_2","model_loss"]
-    data_printing   = {"action":        [True,0,10],
-                       "states_1":      [True,0,4],
-                       "states_2":      [True,0,4],
-                       "model_loss":    [True,0,-1]}
-
-    mem = DataStoring(data_names)
-    for ep in range(num_eps):
-        ep_id = ("ep. %s"%str(ep+1))
-        mem.add_frame(ep_id)
-        for i in range(num_cycles):
-            mem.memorize("action",ep_id,random.uniform(0+(ep*0.5),5+(ep*0.5)))
-            mem.memorize("states_1",ep_id,random.uniform(2-(ep*0.2),4-(ep*0.2)))
-            mem.memorize("states_2",ep_id,random.uniform(0+(ep*0.2),2+(ep*0.2)))
-            mem.memorize("model_loss",ep_id,random.uniform(0.25-(ep*0.02),1-(ep*0.07)))
-            
-    path_save = str(Path.home())
-    mem.save_data(path_save, "data_storage", "\t")
+else:
+    logging     = Log.C_LOG_NOTHING
 
 
-    # 2 Instantiate Stream
-    stream = StreamMLProCSV(p_logging=logging,
-                           p_path_load=path_save,
-                           p_csv_filename="data_storage.csv",
-                           p_delimiter="\t",
-                           p_frame=True,
-                           p_header=True,
-                           p_sampler=SamplerRND(p_max_step_rate=10, p_seed=10),
-                           p_list_features=["action", "states_1", "states_2"],
-                           p_list_labels=["model_loss"])
+# 1 Determine a native data stream provided by MLPro
+stream = StreamMLProRnd10D( p_logging=logging, p_sampler=SamplerRND(p_max_step_rate=10, p_seed=10) )
 
-
-    # 3. load data from the csv file
-    data_names = []
-    mem_from_csv = DataStoring(data_names)
-    mem_from_csv.load_data(path_save, "data_storage.csv", "\t")
-    
-
-    # 4 Performance test: iterate all data streams dark and measure the time
+if __name__ == '__main__':
     input('\nPress ENTER to iterate all streams dark...\n')
 
-    # 4.1 Iterate all instances of the stream
-    tp_start = datetime.now()
-    myiterator = iter(stream)
 
-    stream.switch_logging( p_logging=logging )
-    try:
-        labels = stream.get_label_space().get_num_dim()
-    except:
-        labels = 0
-    stream.log(Log.C_LOG_TYPE_W, 'Features:', stream.get_feature_space().get_num_dim(), ', Labels:', labels, ', Instances:', stream.get_num_instances() )
+# 2 Iterate all instances of the stream
+tp_start = datetime.now()
+myiterator = iter(stream)
 
-    for i, curr_instance in enumerate(myiterator):
-        curr_data = curr_instance.get_feature_data().get_values()
-        
-    myiterator.switch_logging( p_logging=logging )
-    stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
+stream.switch_logging( p_logging=logging )
+try:
+    labels = stream.get_label_space().get_num_dim()
+except:
+    labels = 0
+stream.log(Log.C_LOG_TYPE_W, 'Features:', stream.get_feature_space().get_num_dim(), ', Labels:', labels, ', Instances:', stream.get_num_instances() )
 
-    tp_end       = datetime.now()
-    duration     = tp_end - tp_start
-    duration_sec = ( duration.seconds * 1000000 + duration.microseconds + 1 ) / 1000000
-    rate         = myiterator.get_num_instances() / duration_sec
-    myiterator.log(Log.C_LOG_TYPE_W, 'Done in', round(duration_sec,3), ' seconds (throughput =', round(rate), 'instances/sec)')   
+for i, curr_instance in enumerate(myiterator):
+    curr_data = curr_instance.get_feature_data().get_values()
     
+myiterator.switch_logging( p_logging=logging )
+stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
+
+tp_end       = datetime.now()
+duration     = tp_end - tp_start
+duration_sec = ( duration.seconds * 1000000 + duration.microseconds + 1 ) / 1000000
+rate         = myiterator.get_num_instances() / duration_sec
+myiterator.log(Log.C_LOG_TYPE_W, 'Done in', round(duration_sec,3), ' seconds (throughput =', round(rate), 'instances/sec)')   
+
+
+# 3 Change the sampler method
+
+# 3.1 Weigthed random sampling
+stream._sampler = SamplerWeightedRND(p_threshold=0.75, p_seed=10)
+
+for i, curr_instance in enumerate(myiterator):
+    curr_data = curr_instance.get_feature_data().get_values()
     
-    # 5 Change the sampler method
+myiterator.switch_logging( p_logging=logging )
+stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
 
-    # 5.1 Weigthed random sampling
-    stream._sampler = SamplerWeightedRND(p_threshold=0.75, p_seed=10)
+# 3.2.1 Reservoir sampling with number of instances
+stream._sampler = SamplerReservoir(p_num_instances=stream.get_num_instances(), p_reservoir_size=100, p_seed=10)
 
-    for i, curr_instance in enumerate(myiterator):
-        curr_data = curr_instance.get_feature_data().get_values()
-        
-    myiterator.switch_logging( p_logging=logging )
-    stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
-
-    # 5.2.1 Reservoir sampling with number of instances
-    stream._sampler = SamplerReservoir(p_num_instances=stream.get_num_instances(), p_reservoir_size=100, p_seed=10)
-
-    for i, curr_instance in enumerate(myiterator):
-        curr_data = curr_instance.get_feature_data().get_values()
-        
-    myiterator.switch_logging( p_logging=logging )
-    stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
-
-    # 5.2.2 Reservoir sampling without number of instances
-    stream._sampler = SamplerReservoir(p_reservoir_size=100, p_seed=10)
-
-    for i, curr_instance in enumerate(myiterator):
-        curr_data = curr_instance.get_feature_data().get_values()
-        
-    myiterator.switch_logging( p_logging=logging )
-    stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
-
-    # 5.3 Min-wise sampling
-    stream._sampler = SamplerMinWise(p_cluster_size=20, p_seed=10)
-
-    for i, curr_instance in enumerate(myiterator):
-        curr_data = curr_instance.get_feature_data().get_values()
-        
-    myiterator.switch_logging( p_logging=logging )
-    stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
+for i, curr_instance in enumerate(myiterator):
+    curr_data = curr_instance.get_feature_data().get_values()
     
+myiterator.switch_logging( p_logging=logging )
+stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
+
+# 3.2.2 Reservoir sampling without number of instances
+stream._sampler = SamplerReservoir(p_reservoir_size=100, p_seed=10)
+
+for i, curr_instance in enumerate(myiterator):
+    curr_data = curr_instance.get_feature_data().get_values()
+    
+myiterator.switch_logging( p_logging=logging )
+stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
+
+# 3.3 Min-wise sampling
+stream._sampler = SamplerMinWise(p_cluster_size=20, p_seed=10)
+
+for i, curr_instance in enumerate(myiterator):
+    curr_data = curr_instance.get_feature_data().get_values()
+    
+myiterator.switch_logging( p_logging=logging )
+stream.log(Log.C_LOG_TYPE_W, 'Number of instances being sampled:', int(i+1) )
+
