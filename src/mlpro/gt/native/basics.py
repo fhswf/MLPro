730e7ef029c631db67e8eec93728f929a1d08194
steveyuwono <steveyuwono@gmail.com>
2023-05-17 15:20:22 +0200

    class GTPlayer #613

--- src/mlpro/gt/native/basics.py
+++ src/mlpro/gt/native/basics.py
@@ -48,17 +48,6 @@ class GTPayoffMatrix (TStamp):
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class GTPlayer (GTSolver):
-
-
-## -------------------------------------------------------------------------------------------------
-    def __init__(self):
-        pass
-
-
-
-## -------------------------------------------------------------------------------------------------
-## -------------------------------------------------------------------------------------------------
 class GTMultiPlayer (GTPlayer):
 
 
@@ -71,6 +60,8 @@ class GTMultiPlayer (GTPlayer):
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
 class GTStrategy (Action):
+
+    C_TYPE          = 'GTStrategy'
 
 
 ## -------------------------------------------------------------------------------------------------
@@ -96,7 +87,7 @@ class GTStrategy (Action):
 ## -------------------------------------------------------------------------------------------------
 class GTSolver (Task, ScientificObject):
 
-    C_TYPE          = 'Policy'
+    C_TYPE          = 'GTSolver'
     C_NAME          = '????'
 
     C_SCIREF_TYPE   = ScientificObject.C_SCIREF_TYPE_NONE
@@ -165,6 +156,11 @@ class GTSolver (Task, ScientificObject):
 
 
 ## -------------------------------------------------------------------------------------------------
+    def get_strategy_space(self) -> MSpace:
+        return self._strategy_space
+
+
+## -------------------------------------------------------------------------------------------------
     def set_random_seed(self, p_seed=None):
         """
         Resets the internal random generator using the given seed.
@@ -183,3 +179,72 @@ class GTSolver (Task, ScientificObject):
     def _compute_strategy(self, p_payoff:GTPayoffMatrix) -> GTStrategy:
         
         raise NotImplementedError
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class GTPlayer (GTSolver):
+
+    C_TYPE = 'GTPlayer'
+    C_NAME = ''
+
+
+## -------------------------------------------------------------------------------------------------
+    def __init__(self,
+                 p_solver: GTSolver,
+                 p_name='',
+                 p_visualize:bool=True,
+                 p_logging=Log.C_LOG_ALL,
+                 **p_param):
+
+        if p_name != '':
+            self.set_name(p_name)
+        else:
+            self.set_name(self.C_NAME)
+
+        self._solver = p_solver
+
+        GTSolver.__init__(self,
+                          p_strategy_space = self._solver.get_strategy_space(),
+                          p_id = self._solver.get_id(),
+                          p_visualize = p_visualize,
+                          p_logging = p_logging,
+                          **p_param)
+
+
+## -------------------------------------------------------------------------------------------------
+    def _init_hyperparam(self, **p_param):
+
+        # 1 Create a dispatcher hyperparameter tuple for the player
+        self._hyperparam_tuple = HyperParamDispatcher(p_set=self._hyperparam_space)
+
+        # 2 Extend agent's hp space and tuple from policy
+        try:
+            self._hyperparam_space.append( self._solver.get_hyperparam().get_related_set(), p_new_dim_ids=False)
+            self._hyperparam_tuple.add_hp_tuple(self._solver.get_hyperparam())
+        except:
+            pass
+
+        
+## -------------------------------------------------------------------------------------------------
+    def switch_logging(self, p_logging):
+        super().switch_logging(p_logging)
+        self._solver.switch_logging(p_logging)
+
+
+## -------------------------------------------------------------------------------------------------
+    def get_strategy_space(self) -> MSpace:
+        return self._solver.get_strategy_space()
+
+
+## -------------------------------------------------------------------------------------------------
+    def set_random_seed(self, p_seed=None):
+        self._solver.set_random_seed(p_seed)
+
+
+## -------------------------------------------------------------------------------------------------
+    def compute_strategy(self, p_payoff:GTPayoffMatrix) -> GTStrategy:
+        return self._solver.compute_strategy(p_payoff)
