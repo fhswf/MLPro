f4ca85ee076c5623d08634275fa3a54b554161b6
steveyuwono <steveyuwono@gmail.com>
2023-05-17 14:42:38 +0200

    class GTSolver #613

--- src/mlpro/gt/native/basics.py
+++ src/mlpro/gt/native/basics.py
@@ -16,9 +16,11 @@ This module provides model classes for tasks related to a Native Game Theory.
 """
 
 
-from mlpro.bf.various import TStamp, Persistent
-from mlpro.bf.systems import Action
-from mlpro.bf.ml import Model, Scenario
+from mlpro.bf.various import *
+from mlpro.bf.systems import *
+from mlpro.bf.ml import *
+from mlpro.bf.mt import *
+from mlpro.bf.math import *
 
 
 
@@ -57,17 +59,6 @@ class GTStrategy (Action):
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class GTSolver (Model):
-
-
-## -------------------------------------------------------------------------------------------------
-    def __init__(self):
-        pass
-
-
-
-## -------------------------------------------------------------------------------------------------
-## -------------------------------------------------------------------------------------------------
 class GTPlayer (GTSolver):
 
 
@@ -96,3 +87,97 @@ class GTEvaluation (Persistent):
 ## -------------------------------------------------------------------------------------------------
     def __init__(self):
         pass
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class GTSolver (Task, ScientificObject):
+
+    C_TYPE          = 'Policy'
+    C_NAME          = '????'
+
+    C_SCIREF_TYPE   = ScientificObject.C_SCIREF_TYPE_NONE
+
+
+## -------------------------------------------------------------------------------------------------
+    def __init__(self,
+                 p_strategy_space:MSpace,
+                 p_id = None,
+                 p_visualize:bool = False,
+                 p_logging = Log.C_LOG_ALL,
+                 **p_param):
+        
+        Task.__init__(self,
+                      p_id = p_id,
+                      p_name = None,
+                      p_range_max = Async.C_RANGE_PROCESS,
+                      p_autorun = Task.C_AUTORUN_NONE,
+                      p_class_shared = None,
+                      p_visualize = p_visualize,
+                      p_logging = p_logging)
+
+        self._hyperparam_space  = HyperParamSpace()
+        self._hyperparam_tuple  = None
+        self._init_hyperparam(**p_param)
+
+        self._strategy_space = p_strategy_space
+
+        try:
+            self._setup_solver()
+        except:
+            pass
+
+
+## -------------------------------------------------------------------------------------------------
+    def _init_hyperparam(self, **p_param):
+        """
+        Implementation specific hyperparameters can be added here. Please follow these steps:
+        a) Add each hyperparameter as an object of type HyperParam to the internal hyperparameter
+           space object self._hyperparam_space
+        b) Create hyperparameter tuple and bind to self._hyperparam_tuple
+        c) Set default value for each hyperparameter
+
+        Parameters
+        ----------
+        p_par : Dict
+            Further model specific hyperparameters, that are passed through constructor.
+        """
+
+        pass
+
+
+## -------------------------------------------------------------------------------------------------
+    def _setup_solver(self):
+
+        raise NotImplementedError
+
+
+## -------------------------------------------------------------------------------------------------
+    def get_hyperparam(self) -> HyperParamTuple:
+        """
+        Returns the internal hyperparameter tuple to get access to single values.
+        """
+
+        return self._hyperparam_tuple
+
+
+## -------------------------------------------------------------------------------------------------
+    def set_random_seed(self, p_seed=None):
+        """
+        Resets the internal random generator using the given seed.
+        """
+
+        random.seed(p_seed)
+
+
+## -------------------------------------------------------------------------------------------------
+    def compute_strategy(self, p_payoff:GTPayoffMatrix) -> GTStrategy:
+        
+        return self._compute_strategy(p_payoff)
+
+
+## -------------------------------------------------------------------------------------------------
+    def _compute_strategy(self, p_payoff:GTPayoffMatrix) -> GTStrategy:
+        
+        raise NotImplementedError
