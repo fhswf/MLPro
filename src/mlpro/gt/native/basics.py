b0ac95d67bf5eca3a4200ff89385867784fa9d44
steveyuwono <steveyuwono@gmail.com>
2023-05-17 17:36:18 +0200

    class GTCoalition #613

--- src/mlpro/gt/native/basics.py
+++ src/mlpro/gt/native/basics.py
@@ -48,17 +48,6 @@ class GTPayoffMatrix (TStamp):
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class GTMultiPlayer (GTPlayer):
-
-
-## -------------------------------------------------------------------------------------------------
-    def __init__(self):
-        pass
-
-
-
-## -------------------------------------------------------------------------------------------------
-## -------------------------------------------------------------------------------------------------
 class GTStrategy (Action):
 
     C_TYPE          = 'GTStrategy'
@@ -223,8 +212,8 @@ class GTPlayer (GTSolver):
 
         # 2 Extend agent's hp space and tuple from policy
         try:
-            self._hyperparam_space.append( self._solver.get_hyperparam().get_related_set(), p_new_dim_ids=False)
-            self._hyperparam_tuple.add_hp_tuple(self._solver.get_hyperparam())
+            self._hyperparam_space.append( self.get_solver().get_hyperparam().get_related_set(), p_new_dim_ids=False)
+            self._hyperparam_tuple.add_hp_tuple(self.get_solver().get_hyperparam())
         except:
             pass
 
@@ -232,19 +221,172 @@ class GTPlayer (GTSolver):
 ## -------------------------------------------------------------------------------------------------
     def switch_logging(self, p_logging):
         super().switch_logging(p_logging)
-        self._solver.switch_logging(p_logging)
+        self.get_solver().switch_logging(p_logging)
+
+
+## -------------------------------------------------------------------------------------------------
+    def set_log_level(self, p_level):
+        super().set_log_level(p_level)
+        self.get_solver().set_log_level(p_level)
 
 
 ## -------------------------------------------------------------------------------------------------
     def get_strategy_space(self) -> MSpace:
-        return self._solver.get_strategy_space()
+        return self.get_solver().get_strategy_space()
 
 
 ## -------------------------------------------------------------------------------------------------
     def set_random_seed(self, p_seed=None):
-        self._solver.set_random_seed(p_seed)
+        self.get_solver().set_random_seed(p_seed)
 
 
 ## -------------------------------------------------------------------------------------------------
     def compute_strategy(self, p_payoff:GTPayoffMatrix) -> GTStrategy:
-        return self._solver.compute_strategy(p_payoff)
+        return self.get_solver().compute_strategy(p_payoff)
+
+
+## -------------------------------------------------------------------------------------------------
+    def get_solver(self) -> GTSolver:
+        return self._solver
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class GTCoalition (GTPlayer):
+
+    C_TYPE  = 'GTCoalition'
+    C_NAME  = ''
+
+    C_COALITION_STRATEGY    = None
+    C_COALITION_CONCATENATE = 0
+    C_COALITION_MEAN        = 1
+    C_COALITION_SUM         = 2
+    C_COALITION_MIN         = 3
+    C_COALITION_MAX         = 4
+
+
+## -------------------------------------------------------------------------------------------------
+    def __init__(self,
+                 p_name:str = " ",
+                 p_coalition_type = None,
+                 p_logging = Log.C_LOG_ALL):
+        
+        self._coop_players      = []
+        self._coop_players_ids  = []
+
+        if p_coalition_type is None:
+            raise ParamError("Please add a coalition strategy!")
+        else:
+            self._co_strategy = p_coalition_type
+
+        if p_name != '':
+            self.set_name(p_name)
+        else:
+            self.set_name(self.C_NAME)
+        
+        self.switch_logging(p_logging)
+
+
+## -------------------------------------------------------------------------------------------------
+    def switch_logging(self, p_logging) -> None:
+        Log.switch_logging(self, p_logging=p_logging)
+
+        for pl in self._coop_players:
+            pl.switch_logging(p_logging)
+
+    
+## -------------------------------------------------------------------------------------------------
+    def set_log_level(self, p_level):
+        Log.set_log_level(self, p_level)
+
+        for pl in self._coop_players:
+            pl.set_log_level(p_level)
+
+
+## -------------------------------------------------------------------------------------------------
+    def add_agent(self, p_player:GTPlayer):
+
+        self._coop_players.append(p_player)
+        self._coop_players_ids.append(p_player.get_id())
+
+        self.log(Log.C_LOG_TYPE_I, p_player.C_TYPE + ' ' + p_player.get_name() + ' added.')
+
+        if p_player.get_solver().get_hyperparam() is not None:
+            self._hyperparam_space.append(p_set=p_player._solver.get_hyperparam().get_related_set(),
+                                          p_new_dim_ids=False,
+                                          p_ignore_duplicates=True)
+        
+        if self._hyperparam_tuple is None:
+            self._hyperparam_tuple = HyperParamDispatcher(p_set=self._hyperparam_space)
+            
+        self._hyperparam_tuple.add_hp_tuple(p_player.get_hyperparam())
+                        
+
+## -------------------------------------------------------------------------------------------------
+    def get_players(self) -> list:
+        return self._coop_players
+                        
+
+## -------------------------------------------------------------------------------------------------
+    def get_players_ids(self) -> list:
+        return self._coop_players_ids
+                        
+
+## -------------------------------------------------------------------------------------------------
+    def get_player(self, p_player_id) -> GTPlayer:
+        return self._coop_players[self._coop_players_ids.index(p_player_id)]
+                        
+
+## -------------------------------------------------------------------------------------------------
+    def get_coaltion_strategy(self) -> int:
+        return self._co_strategy
+
+    
+## -------------------------------------------------------------------------------------------------
+    def set_random_seed(self, p_seed=None):
+        for pl in self._coop_players:
+            pl.set_random_seed(p_seed)
+
+
+## -------------------------------------------------------------------------------------------------
+    def get_strategy_space(self) -> ESpace:
+        if self.get_coaltion_strategy == 0:
+            return None
+        else:
+            espace = ESpace()
+            espace.add_dim(Dimension( p_name_short='CoStr', p_name_long='Coalition Strategy', p_boundaries=[-np.inf,np.inf]))
+            return espace
+
+
+## -------------------------------------------------------------------------------------------------
+    def compute_strategy(self, p_payoff:GTPayoffMatrix) -> GTStrategy:
+
+        strategy = GTStrategy()
+
+        for pl in self._coop_players:
+            strategy_pl     = pl.compute_strategy(p_payoff)
+            strategy_elem   = strategy_pl.get_elem(pl.get_id())
+            strategy.add_elem(pl.get_id(), strategy_elem)
+
+        if self.get_coaltion_strategy == 0:
+            return strategy
+        else:
+            arr = strategy.get_sorted_values()
+
+            if self.get_coaltion_strategy == 1:
+                value = arr.mean()
+            elif self.get_coaltion_strategy == 2:
+                value = arr.sum()
+            elif self.get_coaltion_strategy == 3:
+                value = arr.min()
+            elif self.get_coaltion_strategy == 4:
+                value = arr.max()
+
+            coalition_strategy = GTStrategy(self.get_id(), Element(self.get_strategy_space), value)
+            return coalition_strategy
+    
+
+
