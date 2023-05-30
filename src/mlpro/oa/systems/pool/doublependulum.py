bdccd4500e6e2fe7f56e3aee6de70ea0a8a3b667
Laxmikant Shrikant Baheti <100450659+laxmikantbaheti@users.noreply.github.com>
2023-05-30 12:16:18 +0200

    RL/OA: Double Pendulum - Adaptive extensions #466

---
+++ src/mlpro/oa/systems/pool/doublependulum.py
@@ -0,0 +1,19 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro.oa.systems.pool
+## -- Module  : doublependulum.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2023-mm-mm  0.0.0     LSB      Creation
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 0.0.0 (2023-dd-mm)
+
+This module provides the adaptive extensions of the Double Pendulum System.
+
+"""
+
+from mlpro.bf.systems import *
+from mlpro.bf.systems.pool.doublependulum import *
