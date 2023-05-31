57050011a8f2b58e9bda60cce9fe5ae79403eded
Laxmikant Shrikant Baheti <100450659+laxmikantbaheti@users.noreply.github.com>
2023-05-31 16:39:06 +0200

    RL/OA: Howto for Double Pendulum #459

--- src/mlpro/rl/examples/howto_oarl_010_doublependulum_with_boudarydetector.py
+++ src/mlpro/rl/examples/howto_oarl_010_doublependulum_with_boudarydetector.py
@@ -5,40 +5,58 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
-## -- 2023-05-31  1.0.0     LSB      Creation
+## -- 2023-05-31  1.0.0     LSB      Creation/release
 ## -------------------------------------------------------------------------------------------------
 
 """
 Ver. 1.0.0 (2023-01-12)
-...
+
+This module is an illustration of howto run the online adaptive double pendulum (OADP) environment.
 
 You will learn:
 
-1.
+1. How to set up the OADP environment
 
-2.
+2. How to create online adaptive tasks
 
-3.
+3. How to add the tasks to the reward workflow of the OADP environment
 
 """
 
 
 from mlpro.rl.pool.envs.doublependulum import DoublePendulumOA7
 from mlpro.rl import *
-from mlpro.oa.streams.tasks import BoundaryDetector
+from mlpro.oa.streams.tasks import BoundaryDetector, NormalizerMinMax
 from mlpro.rl.pool.policies.randomgenerator import RandomGenerator
 
 
 
-
+# Creating the Online Adaptive Double Pendulum Environment
 environment = DoublePendulumOA7(p_name = '', p_ada=True, p_visualize=True)
 
+# Creating the Boundary Detector Task
 task_bd = BoundaryDetector(p_name='Boundary Detector', p_visualize=True, p_range_max=Range.C_RANGE_NONE)
 
+# Creating the Normalizer Task
+task_norm = NormalizerMinMax(p_name='Normalizer', p_visualize=True, p_range_max=Range.C_RANGE_NONE)
+
+# Adding the boundary detector task to the Reward Workflow
 environment.add_task_reward(p_task=task_bd)
 
+# Adding the normalizer task to the reward workflow
+environment.add_task_reward(p_task=task_norm, p_pred_tasks=[task_bd])
+
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 class OADPScenario(RLScenario):
 
+
+## -------------------------------------------------------------------------------------------------
     def _setup(self, p_mode, p_ada: bool, p_visualize: bool, p_logging) -> Model:
         # 1.1 Setup environment
         self._env   = environment
@@ -62,6 +80,29 @@ class OADPScenario(RLScenario):
         )
 
 
-scnenario = OADPScenario(p_cycle_limit=100)
 
-scnenario.run()
+# 2 Create scenario and run the scenario
+if __name__ == "__main__":
+    # 2.1 Parameters for demo mode
+    cycle_limit         = 300
+    logging             = Log.C_LOG_ALL
+    visualize           = True
+    plotting            = True
+else:
+    # 2.2 Parameters for unittest
+    cycle_limit         = 2
+    logging             = Log.C_LOG_NOTHING
+    visualize           = False
+    plotting            = False
+
+
+
+# 3 Create your scenario and run some cycles
+myscenario  = OADPScenario( p_mode=Mode.C_MODE_SIM,
+                            p_ada=True,
+                            p_cycle_limit=cycle_limit,
+                            p_visualize=visualize,
+                            p_logging=logging )
+
+myscenario.reset(p_seed=3)
+myscenario.run()
