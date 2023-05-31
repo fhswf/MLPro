167eb7ec966fab1abef3d58e60ca75adb0cf986b
Laxmikant Shrikant Baheti <100450659+laxmikantbaheti@users.noreply.github.com>
2023-05-31 15:51:30 +0200

    RL/OA: Howto for Double Pendulum #459

---
+++ src/mlpro/rl/examples/howto_oarl_010_doublependulum_with_boudarydetector.py
@@ -0,0 +1,67 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro.rl.examples
+## -- Module  : howto_rl_oarl_999_adaptive_environment.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2023-05-31  1.0.0     LSB      Creation
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 1.0.0 (2023-01-12)
+...
+
+You will learn:
+
+1.
+
+2.
+
+3.
+
+"""
+
+
+from mlpro.rl.pool.envs.doublependulum import DoublePendulumOA7
+from mlpro.rl import *
+from mlpro.oa.streams.tasks import BoundaryDetector
+from mlpro.rl.pool.policies.randomgenerator import RandomGenerator
+
+
+
+
+environment = DoublePendulumOA7(p_name = '', p_ada=True, p_visualize=True)
+
+task_bd = BoundaryDetector(p_name='Boundary Detector', p_visualize=True, p_range_max=Range.C_RANGE_NONE)
+
+environment.add_task_reward(p_task=task_bd)
+
+class OADPScenario(RLScenario):
+
+    def _setup(self, p_mode, p_ada: bool, p_visualize: bool, p_logging) -> Model:
+        # 1.1 Setup environment
+        self._env   = environment
+
+
+        # 1.2 Setup and return random action agent
+        policy_random = RandomGenerator(p_observation_space=self._env.get_state_space(),
+                                        p_action_space=self._env.get_action_space(),
+                                        p_buffer_size=1,
+                                        p_ada=True,
+                                        p_visualize=True,
+                                        p_logging=p_logging)
+
+        return Agent(
+            p_policy=policy_random,
+            p_envmodel=None,
+            p_name='Smith',
+            p_ada=p_ada,
+            p_visualize=True,
+            p_logging=p_logging
+        )
+
+
+scnenario = OADPScenario(p_cycle_limit=100)
+
+scnenario.run()
