7d9d80e3765c50eeceeaa529fa8c17c10150c353
detlefarend <detlef@arend-privat.net>
2023-06-09 14:30:04 +0200

    #768

---
+++ src/mlpro/wrappers/sklearn/anomalydetectors.py
@@ -0,0 +1,27 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro.wrappers.sklearn
+## -- Module  : anomalydetectors.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2023-06-09  0.0.0     DA      Creation
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 0.0.0 (2023-06-09)
+
+This module provides wrapper functionalities to incorporate anomaly detector algorithms of the 
+Scikit-learn ecosystem.
+
+Learn more:
+https://scikit-learn.org
+
+"""
+
+
+from mlpro.wrappers.sklearn.basics import *
+
+
+
+
