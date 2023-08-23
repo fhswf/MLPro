c3d9bfbbfcb325736880c3caf0e3620e8214038f
steveyuwono <steveyuwono@gmail.com>
2023-08-23 11:31:29 +0200

    creation of howto_oa_ca_001_clustream.py

---
+++ src/mlpro/oa/examples/howto_oa_ca_001_clustream.py
@@ -0,0 +1,27 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro.oa.examples
+## -- Module  : howto_oa_ca_001_clustream.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2023-08-23  0.0.0     SY       Creation
+## -- 2023-08-XX  1.0.0     SY       First version release
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 1.0.0 (2023-08-XX)
+
+This module demonstrates the combination of several tasks in a workflow, which includes:
+
+1) Boundary Detector,
+
+2) Min/Max-Normalizer, and
+
+3) Wrapped CluStream Algorithm (River).
+
+Two data stream are incorporated in this module, such as static 3D point clouds and dynamic 3D point
+clouds. This module is prepared for the MLPro-OA scientific paper and going to be stored as Code
+Ocean Capsule, thus the result is reproducible.
+
+"""
