78f9330c52fcb752ab9b9b07d974d33f067c91ae
detlefarend <detlef@arend-privat.net>
2023-09-08 14:06:06 +0200

    Marketplace for MLPro extensions #413

--- marketplace/update_marketplace.py
+++ marketplace/update_marketplace.py
@@ -1,1 +1,36 @@
-print("Hello World")
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro Marketplace
+## -- Module  : update_marketplace.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2023-09-08  0.0.0     DA       Creation 
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 0.0.0 (2023-09-08)
+
+This standalone module collects meta data of all whitelisted GitHub repositories based on the
+template repo /fhswf/MLPro-Extension.
+"""
+
+
+from mlpro.bf.various import *
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class Marketplace (Log):
+
+    C_TYPE      = 'Marketplace'
+    C_NAME      = 'MLPro'
+
+## -------------------------------------------------------------------------------------------------
+    def __init__(self, p_logging=Log.C_LOG_ALL):
+        super().__init__(p_logging)
+
+
+
+
+marketplace = Marketplace()
