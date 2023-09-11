c2301c1744d3a0cd67aff6aa9d3773eaaeecb015
detlefarend <detlef@arend-privat.net>
2023-09-11 10:40:45 +0200

    Marketplace for MLPro extensions #413

--- marketplace/update_marketplace.py
+++ marketplace/update_marketplace.py
@@ -6,10 +6,11 @@
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
 ## -- 2023-09-08  0.0.0     DA       Creation 
 ## -- 2023-09-08  0.1.0     DA       Basic structure
+## -- 2023-09-11  0.2.0     DA       Method Marketplace._get_extensions implemented
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 0.1.0 (2023-09-08)
+Ver. 0.2.0 (2023-09-11)
 
 This standalone module collects meta data of all whitelisted GitHub repositories based on the
 template repo /fhswf/MLPro-Extension.
@@ -33,16 +34,20 @@ class Marketplace (Log):
     C_FNAME_WHITELIST   = 'whitelist'
     C_FNAME_BLACKLIST   = 'blacklist'
     C_FNAME_DB          = 'marketplace.csv'
-
+    
 ## -------------------------------------------------------------------------------------------------
-    def __init__(self, p_logging=Log.C_LOG_ALL):
+    def __init__(self, p_token, p_logging=Log.C_LOG_ALL):
         super().__init__(p_logging)
 
-        self.gh = Github( auth = Auth.Token("access_token"))
+        if p_token is not None:
+            self.gh = Github( auth = Auth.Token(p_token) )
+        else:
+            self.gh = None
+            self.log(Log.C_LOG_TYPE_E, 'Please provide valid Github access token as first parameter')
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _get_new_extensions(self):
+    def _get_extensions(self):
         """
         This method gets all repositories based on the template MLPro-Extension that are neigther
         whitelisted nor blacklisted.
@@ -50,13 +55,53 @@ class Marketplace (Log):
 
         # 0 Intro
         self.log(Log.C_LOG_TYPE_S, 'Start scanning for new extensions...')
+        extensions  = []
+        whitelist   = {}
 
 
-        # 1 Scanning for all Github repositories based on template MLPro-Extension
-        extensions = self.gh.search_repositories( query='MLPro in:readme')
+        # 1 Get whitelisted extensions
+        with open( sys.path[0] + os.sep + self.C_FNAME_WHITELIST ) as f:
+            for repo in f.readlines():
+                whitelist[repo] = 'Approved'
+
+        # 2 Get blacklisted extensions
+        with open( sys.path[0] + os.sep + self.C_FNAME_BLACKLIST ) as f:
+            for repo in f.readlines():
+                whitelist[repo] = 'Denied'
 
 
-        # 2 Outro
+        # 3 Scanning for all Github repositories based on template MLPro-Extension
+        results = self.gh.search_repositories(query='mlpro-extension in:topics')        
+
+        for repo in results:
+            if repo.is_template or repo.private: continue
+            try:
+                latest_release = repo.get_latest_release()
+            except:
+                continue
+
+            try:
+                status = whitelist[repo.full_name]
+            except:
+                status = 'Pending'
+
+            extensions.append( ( repo.full_name,
+                                 status,
+                                 repo.name,
+                                 repo.description, 
+                                 latest_release.tag_name,
+                                 latest_release.title,
+                                 latest_release.last_modified,
+                                 repo.html_url,
+                                 repo.homepage,
+                                 repo.topics,
+                                 repo.organization.name,
+                                 repo.organization.location,
+                                 repo.organization.login,
+                                 repo.organization.html_url) )
+
+
+        # 4 Outro
         self.log(Log.C_LOG_TYPE_S, 'End of scanning for new extensions')
         return extensions
     
@@ -75,22 +120,6 @@ class Marketplace (Log):
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _get_approved_extensions(self):
-        """
-        This method gets all whitelisted MLPro extensions that are not explicitely blacklisted.
-        """
-
-        # 0 Intro
-        self.log(Log.C_LOG_TYPE_S, 'Start getting approved extensions...')
-
-        # 1 Reporting
-        self.log(Log.C_LOG_TYPE_E, 'Not yet implemented')
-
-        # 2 Outro
-        self.log(Log.C_LOG_TYPE_S, 'End of getting approved extensions')
-
-
-## -------------------------------------------------------------------------------------------------
     def _build_marketplace_db(self, p_approved_extensions : list):
 
         # 0 Intro
@@ -104,82 +133,31 @@ class Marketplace (Log):
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _update_db(self):
-
-        # 0 Intro
-        self.log(Log.C_LOG_TYPE_S, 'Start updating the marketplace DB...')
-
-
-        # 1 Check whether the whitelist file is touched after the marketplace database
-        tstamp_whitelist    = None
-        tstamp_blacklist    = None
-        tstamp_db           = None
-
-        # 1.1 Get modification time of whitelist file
-        fname_whitelist  = sys.path[0] + os.sep + self.C_FNAME_WHITELIST
-        self.log(Log.C_LOG_TYPE_I, 'Whitelist file:', fname_whitelist)        
-        try:
-            tstamp_whitelist = os.path.getmtime(fname_whitelist)
-            self.log(Log.C_LOG_TYPE_I, 'Whitelist touched at', time.ctime(tstamp_whitelist))
-        except:
-            # Whitelist file not found -> Abort
-            self.log(Log.C_LOG_TYPE_E, 'Whitelist file', fname_whitelist, 'not found')
-            return
-
-
-        # 1.2 Get modification time of blacklist file
-        fname_blacklist  = sys.path[0] + os.sep + self.C_FNAME_BLACKLIST
-        self.log(Log.C_LOG_TYPE_I, 'Blacklist file:', fname_blacklist)        
-        try:
-            tstamp_blacklist = os.path.getmtime(fname_blacklist)
-            self.log(Log.C_LOG_TYPE_I, 'Blacklist touched at', time.ctime(tstamp_blacklist))
-        except:
-            # Blacklist file not found
-            self.log(Log.C_LOG_TYPE_W, 'Blacklist file', fname_blacklist, 'not found')
-
-
-        # 1.3 Get modification time of marketplace db
-        fname_db  = sys.path[0] + os.sep + self.C_FNAME_DB
-        self.log(Log.C_LOG_TYPE_I, 'Database file:', fname_db)        
-        try:
-            tstamp_db = os.path.getmtime(fname_db)
-            self.log(Log.C_LOG_TYPE_I, 'DB touched at', time.ctime(tstamp_db))
-        except:
-            # DB file not found
-            self.log(Log.C_LOG_TYPE_W, 'DB file', fname_db, 'not found')
-        
-
-        # 1.4 Check: DB file up-to-date?
-        if ( tstamp_db is not None ) and ( tstamp_whitelist < tstamp_db ) and ( ( tstamp_blacklist is None ) or ( tstamp_blacklist < tstamp_db) ):
-            self.log(Log.C_LOG_TYPE_S, 'Marketplace DB is still up-to-date -> END')
-            return
-        
-
-        # 2 Build marketplace DB file based on approved MLPro extensions
-        self._build_marketplace_db( self._get_approved_extensions() )
-
-
-        # 3 Outro
-        self.log(Log.C_LOG_TYPE_S, 'End of updating the marketplace DB')
-
-
-## -------------------------------------------------------------------------------------------------
     def update(self):
+
+        # 0 Check: Github access enabled?
+        if self.gh is None: return
+
 
         # 1 Report new MLPro extensions  
         self.log(Log.C_LOG_TYPE_S, 'Step 1: Determination and report of new MLPro extensions...')   
-        new_extensions = self._get_new_extensions()
-        if len(new_extensions) == 0:
+        extensions = self._get_extensions()
+        if len(extensions) == 0:
             self.log(Log.C_LOG_TYPE_I, 'No new MLPro extensions detected')
         else:
-            self.log(Log.C_LOG_TYPE_I, 'New MLPro extensions detected: ', new_extensions)
-            self._report_new_extensions(new_extensions)
+            self.log(Log.C_LOG_TYPE_I, 'New MLPro extensions detected: ', extensions)
+            self._report_new_extensions(extensions)
 
 
-        # 2 Update of MLPro marketplace
+        # 2 Build of MLPro marketplace DB file
         self.log(Log.C_LOG_TYPE_S, 'Step 2: Update of MLPro marketplace DB')   
-        self._update_db()
+        self._build_marketplace_db( extensions )
 
 
-        
-Marketplace().update()
+
+try:
+    token = sys.argv[1]
+except:
+    token = None
+
+Marketplace( p_token=token ).update()
