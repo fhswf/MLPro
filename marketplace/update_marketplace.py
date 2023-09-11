963f26c3a7309ff4ba4116cc4d44cb46e6cd38af
detlefarend <detlef@arend-privat.net>
2023-09-11 18:39:38 +0200

    Marketplace for MLPro extensions #413

--- marketplace/update_marketplace.py
+++ marketplace/update_marketplace.py
@@ -35,6 +35,10 @@ class Marketplace (Log):
     C_FNAME_BLACKLIST   = 'blacklist'
     C_FNAME_DB          = 'marketplace.csv'
     
+    C_STATUS_APPROVED   = 'Approved'
+    C_STATUS_DENIED     = 'Denied'
+    C_STATUS_PENDING    = 'Pending'
+    
 ## -------------------------------------------------------------------------------------------------
     def __init__(self, p_token, p_logging=Log.C_LOG_ALL):
         super().__init__(p_logging)
@@ -55,19 +59,22 @@ class Marketplace (Log):
 
         # 0 Intro
         self.log(Log.C_LOG_TYPE_S, 'Start scanning for new extensions...')
-        extensions  = []
-        whitelist   = {}
+        extensions                          = {}
+        extensions[self.C_STATUS_APPROVED]  = {}
+        extensions[self.C_STATUS_DENIED]    = {}
+        extensions[self.C_STATUS_PENDING]   = {}
+        whitelist                           = {}
 
 
         # 1 Get whitelisted extensions
         with open( sys.path[0] + os.sep + self.C_FNAME_WHITELIST ) as f:
             for repo in f.readlines():
-                whitelist[repo] = 'Approved'
+                whitelist[repo] = self.C_STATUS_APPROVED
 
         # 2 Get blacklisted extensions
         with open( sys.path[0] + os.sep + self.C_FNAME_BLACKLIST ) as f:
             for repo in f.readlines():
-                whitelist[repo] = 'Denied'
+                whitelist[repo] = self.C_STATUS_DENIED
 
 
         # 3 Scanning for all Github repositories based on template MLPro-Extension
@@ -83,22 +90,27 @@ class Marketplace (Log):
             try:
                 status = whitelist[repo.full_name]
             except:
-                status = 'Pending'
+                status = self.C_STATUS_PENDING
 
-            extensions.append( ( repo.full_name,
-                                 status,
-                                 repo.name,
-                                 repo.description, 
-                                 latest_release.tag_name,
-                                 latest_release.title,
-                                 latest_release.last_modified,
-                                 repo.html_url,
-                                 repo.homepage,
-                                 repo.topics,
-                                 repo.organization.name,
-                                 repo.organization.location,
-                                 repo.organization.login,
-                                 repo.organization.html_url) )
+            try:
+                org = extensions[status][repo.organization.login]
+            except:
+                org = {}
+                org['name']     = repo.organization.name
+                org['location'] = repo.organization.location
+                org['html_url'] = repo.organization.html_url
+                org['repos']    = []
+                extensions[status][repo.organization.login] = org
+
+            org['repos'].append( ( repo.full_name,
+                                   repo.name,
+                                   repo.description, 
+                                   latest_release.tag_name,
+                                   latest_release.title,
+                                   latest_release.last_modified,
+                                   repo.html_url,
+                                   repo.homepage,
+                                   repo.topics) )
 
 
         # 4 Outro
@@ -107,16 +119,31 @@ class Marketplace (Log):
     
 
 ## -------------------------------------------------------------------------------------------------
-    def _report_new_extensions(self, p_new_extensions : list):
+    def _report_pending_extensions(self, p_new_extensions : list):
 
         # 0 Intro
-        self.log(Log.C_LOG_TYPE_S, 'Start reporting new extensions...')
+        self.log(Log.C_LOG_TYPE_S, 'Start reporting pending extensions...')
+
+
+        # 1 Get MLPro's administrators
+        repo_mlpro          = self.gh.get_repo( 'fhswf/mlpro')
+        mlpro_team          = repo_mlpro.get_teams()
+        mlpro_collaborators = repo_mlpro.get_collaborators()
+
+        for team in mlpro_team:
+            print('Team: ', team)
+            members = team.get_members(role='Maintainer')
+            for member in members:
+                print(member)
+                # print(repo_mlpro.get_collaborator_permission(member))
+                print('\n')
+
 
         # 1 Reporting
         self.log(Log.C_LOG_TYPE_E, 'Not yet implemented')
 
         # 2 Outro
-        self.log(Log.C_LOG_TYPE_S, 'End of report of new extensions')
+        self.log(Log.C_LOG_TYPE_S, 'End of report of pending extensions')
 
 
 ## -------------------------------------------------------------------------------------------------
@@ -139,19 +166,23 @@ class Marketplace (Log):
         if self.gh is None: return
 
 
-        # 1 Report new MLPro extensions  
-        self.log(Log.C_LOG_TYPE_S, 'Step 1: Determination and report of new MLPro extensions...')   
+        # 1 Determine all MLPro extensions
         extensions = self._get_extensions()
-        if len(extensions) == 0:
-            self.log(Log.C_LOG_TYPE_I, 'No new MLPro extensions detected')
+
+
+        # 2 Report pending MLPro extensions
+        pending = extensions[self.C_STATUS_PENDING]
+        if len( pending.keys() ) > 0:
+            self._report_pending_extensions(pending)
         else:
-            self.log(Log.C_LOG_TYPE_I, 'New MLPro extensions detected: ', extensions)
-            self._report_new_extensions(extensions)
+            self.log(Log.C_LOG_TYPE_I, 'No pending MLPro extensions detected')
 
 
-        # 2 Build of MLPro marketplace DB file
-        self.log(Log.C_LOG_TYPE_S, 'Step 2: Update of MLPro marketplace DB')   
-        self._build_marketplace_db( extensions )
+        # 3 Build of MLPro marketplace DB file
+        self._build_marketplace_db( extensions[self.C_STATUS_APPROVED] )
+        
+        
+
 
 
 
