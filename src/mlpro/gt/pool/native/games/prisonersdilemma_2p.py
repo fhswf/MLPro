9ffac2fd21916cc5ede354abe7951e15d20f5a40
steveyuwono <steveyuwono@gmail.com>
2023-09-21 15:35:25 +0200

    GT-Native: Add 2p game of Prisonners' Dilemma #758

---
+++ src/mlpro/gt/pool/native/games/prisonersdilemma_2p.py
@@ -0,0 +1,21 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro.pool.native.games
+## -- Module  : prisonnersdilemma_2p
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2023-09-21  0.0.0     SY       Creation
+## -- 2023-09-21  1.0.0     SY       Release of first version
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 1.0.0 (2023-09-21)
+
+This module provides a 2-player game of Prisonners' Dilemma with random solver. In the near future,
+we are going to add more solvers and this howto is going to be updated accordingly.
+"""
+
+from mlpro.gt.native.basics import *
+from mlpro.gt.pool.native.solvers.randomsolver import RandomSolver
+
