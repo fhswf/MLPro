9eae6a1232e6e497573d0d0d31acf7cdeb65c5ec
steveyuwono <steveyuwono@gmail.com>
2023-12-12 13:10:15 +0100

    add supply demand games 3p #758 #776

--- src/mlpro/gt/pool/native/games/supplydemand_3p.py
+++ src/mlpro/gt/pool/native/games/supplydemand_3p.py
@@ -56,11 +56,11 @@ class TransferFunction_SD3P(TransferFunction):
 
         if self.get_type() == self.C_TRF_FUNC_CUSTOM:
             try:
-                self.demand         = p_args['demand']
-                self.price_seller_1 = p_args['price_seller_1']
-                self.price_seller_2 = p_args['price_seller_2']
-                self.price_seller_3 = p_args['price_seller_3']
-                self.prod_cost      = p_args['prod_cost']
+                self.demand         = p_args['p_demand']
+                self.price_seller_1 = p_args['p_price_seller_1']
+                self.price_seller_2 = p_args['p_price_seller_2']
+                self.price_seller_3 = p_args['p_price_seller_3']
+                self.prod_cost      = p_args['p_prod_cost']
             except:
                 raise NotImplementedError('One/More parameters for this function is missing.')           
         return True
@@ -82,8 +82,8 @@ class TransferFunction_SD3P(TransferFunction):
         else:
 
             price_seller_1  = self.price_seller_1[int(p_input[0]-1)]
-            price_seller_2  = self.price_seller_2[int(p_input[0]-1)]
-            price_seller_3  = self.price_seller_3[int(p_input[0]-1)]
+            price_seller_2  = self.price_seller_2[int(p_input[1]-1)]
+            price_seller_3  = self.price_seller_3[int(p_input[2]-1)]
             list_of_prices  = [price_seller_1, price_seller_2, price_seller_3]
             
             sold_quantity   = 0
@@ -118,7 +118,19 @@ class PayoffFunction_SD3P (GTFunction):
 ## -------------------------------------------------------------------------------------------------
     def _setup_transfer_functions(self):
 
-        raise NotImplementedError
+        TF = TransferFunction_SD3P(
+            p_name="TransferFunction_SD3P",
+            p_type=TransferFunction.C_TRF_FUNC_CUSTOM,
+            p_demand=10,
+            p_price_seller_1=[15,12,9,6,3],
+            p_price_seller_2=[10,8,6,4,2],
+            p_price_seller_3=[8,7,6,5,4],
+            p_prod_cost=5
+            )
+
+        self._add_transfer_function(p_idx=0, p_transfer_fct=TF)
+        self._add_transfer_function(p_idx=1, p_transfer_fct=TF)
+        self._add_transfer_function(p_idx=2, p_transfer_fct=TF)
 
 
 
@@ -131,20 +143,89 @@ class PayoffMatrix_SD3P (GTPayoffMatrix):
 
 ## -------------------------------------------------------------------------------------------------
     def _call_mapping(self, p_input:str, p_strategies:GTStrategy) -> float:
-        
-        raise NotImplementedError
+
+        self._elem_ids  = p_strategies.get_elem_ids()
+        idx             = self._elem_ids.index(p_input)
+
+        return self._function(p_input, p_strategies)[idx]
 
 
 ## -------------------------------------------------------------------------------------------------
     def _call_best_response(self, p_element_id:str) -> float:
+
+        idx = self._elem_ids.index(p_element_id)
+        tf  = self._function._payoff_map[idx]
+
+        if idx == 0:
+            prices = tf.price_seller_1
+        elif idx == 1:
+            prices = tf.price_seller_2
+        elif idx == 2:
+            prices = tf.price_seller_3
         
-        raise NotImplementedError
+        response = []
+        for x, y in enumerate(prices):
+            response.append(y*(x+1))
+        
+        return max(response)
+
 
 
 ## -------------------------------------------------------------------------------------------------
     def _call_zero_sum(self) -> bool:
         
-        raise NotImplementedError
+        return False
+         
+        
+        
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class MaxGreedyPolicy_SD3P_P1(MaxGreedyPolicy):
+
+
+## -------------------------------------------------------------------------------------------------
+    def _call_compute_strategy(self, p_payoff:GTPayoffMatrix) -> GTStrategy:
+        
+        stg_values  = np.zeros(self._strategy_space.get_num_dim())
+
+        tf          = p_payoff._function._payoff_map[0]
+        prices      = tf.price_seller_1
+        
+        response    = []
+        for x, y in enumerate(prices):
+            response.append(y*(x+1))
+        
+        stg_values[0] = response.index(max(response))+1
+        
+        return GTStrategy(self._id, self._strategy_space, stg_values)
+         
+        
+        
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class MaxGreedyPolicy_SD3P_P2(MaxGreedyPolicy):
+
+
+## -------------------------------------------------------------------------------------------------
+    def _call_compute_strategy(self, p_payoff:GTPayoffMatrix) -> GTStrategy:
+        
+        stg_values  = np.zeros(self._strategy_space.get_num_dim())
+
+        tf      = p_payoff._function._payoff_map[1]
+        prices  = tf.price_seller_2
+        
+        response = []
+        for x, y in enumerate(prices):
+            response.append(y*(x+1))
+        
+        stg_values[0] = response.index(max(response))+1
+        
+        return GTStrategy(self._id, self._strategy_space, stg_values)
+
          
         
         
@@ -160,7 +241,95 @@ class SupplyDemand_3P (GTGame):
 ## -------------------------------------------------------------------------------------------------
     def _setup(self, p_mode, p_ada:bool, p_visualize:bool, p_logging) -> Model:
         
-        raise NotImplementedError
+        _strategy_space = MSpace()
+        _strategy_space.add_dim(Dimension('RStr','Z','Strategy','','','',[1,5]))
+        
+        solver1 = MaxGreedyPolicy_SD3P_P1(
+            p_strategy_space=_strategy_space,
+            p_id=0,
+            p_name="Max Greedy Solver",
+            p_visualize=p_visualize,
+            p_logging=p_logging
+        )
+
+
+        p1 = GTPlayer(
+            p_solver=solver1,
+            p_name="Seller 1",
+            p_visualize=p_visualize,
+            p_logging=p_logging,
+            p_random_solver=True
+        )
+
+        coal1 = GTCoalition(
+            p_name="Coalition of Seller 1",
+            p_coalition_type=GTCoalition.C_COALITION_SUM
+        )
+        coal1.add_player(p1)
+        
+        solver2 = MaxGreedyPolicy_SD3P_P2(
+            p_strategy_space=_strategy_space,
+            p_id=1,
+            p_name="Max Greedy Solver",
+            p_visualize=p_visualize,
+            p_logging=p_logging
+        )
+
+
+        p2 = GTPlayer(
+            p_solver=solver2,
+            p_name="Seller 2",
+            p_visualize=p_visualize,
+            p_logging=p_logging,
+            p_random_solver=True
+        )
+
+        coal2 = GTCoalition(
+            p_name="Coalition of Seller 2",
+            p_coalition_type=GTCoalition.C_COALITION_SUM
+        )
+        coal2.add_player(p2)
+        
+        solver3 = RandomSolver(
+            p_strategy_space=_strategy_space,
+            p_id=2,
+            p_visualize=p_visualize,
+            p_logging=p_logging
+        )
+
+        p3 = GTPlayer(
+            p_solver=solver3,
+            p_name="Seller 3",
+            p_visualize=p_visualize,
+            p_logging=p_logging,
+            p_random_solver=True
+        )
+
+        coal3 = GTCoalition(
+            p_name="Coalition of Seller 3",
+            p_coalition_type=GTCoalition.C_COALITION_SUM
+        )
+        coal3.add_player(p3)
+
+        competition = GTCompetition(
+            p_name="Supply Demand 3 Sellers Competition",
+            p_logging=p_logging
+            )
+        competition.add_coalition(coal1)
+        competition.add_coalition(coal2)
+        competition.add_coalition(coal3)
+        
+        coal_ids = competition.get_coalitions_ids()
+
+        self._payoff = PayoffMatrix_SD3P(
+            p_function=PayoffFunction_SD3P(
+                p_func_type=GTFunction.C_FUNC_TRANSFER_FCTS
+                ),
+            p_player_ids=coal_ids
+        )
+        
+        return competition
+        
         
         
 
