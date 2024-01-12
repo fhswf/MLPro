062c2376f0daee02c69d354b5aa216acb4374ead
steveyuwono <steveyuwono@gmail.com>
2024-01-12 15:07:04 +0100

    refactoring: module name

--- src/mlpro/gt/pool/native/solvers/greedypolicy.py
+++ src/mlpro/gt/pool/native/solvers/greedypolicy.py
@@ -1,16 +1,17 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
 ## -- Package : mlpro.pool.native.solvers
-## -- Module  : greedypolicy
+## -- Module  : greedypolicy.py
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
 ## -- 2023-12-07  0.0.0     SY       Creation
 ## -- 2023-12-12  1.0.0     SY       Release of first version
+## -- 2024-01-12  1.0.1     SY       Refactoring: Module Name
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 1.0.0 (2023-12-12)
+Ver. 1.0.1 (2024-01-12)
 
 This module provides solver with greedy GT strategy. There are two variants, such as minimum greedy
 and maximum greedy.
