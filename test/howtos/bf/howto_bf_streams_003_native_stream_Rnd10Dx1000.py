d24280cee74b89ac20d4bf9c80dd95e1bbe99dcf
detlefarend <detlef@arend-privat.net>
2024-05-21 14:32:26 +0200

    Refact: StreamTask, OATask - order of processing/adaption on new/obsolete instances #988

--- test/howtos/bf/howto_bf_streams_003_native_stream_Rnd10Dx1000.py
+++ test/howtos/bf/howto_bf_streams_003_native_stream_Rnd10Dx1000.py
@@ -68,7 +68,7 @@ class MyScenario (StreamScenario):
 # 1 Preparation of demo/unit test mode
 if __name__ == "__main__":
     # 1.1 Parameters for demo mode
-    cycle_limit = 200
+    cycle_limit = 250
     logging     = Log.C_LOG_ALL
     visualize   = True
   
