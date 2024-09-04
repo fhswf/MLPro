5dac62c604e38e30308d677cc4fe89b4e2025ede
detlefarend <detlef@arend-privat.net>
2024-09-04 18:34:39 +0200

    BF: Basics of closed-loop control #1046

--- src/mlpro/bf/control/basics.py
+++ src/mlpro/bf/control/basics.py
@@ -6,16 +6,22 @@
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
 ## -- 2024-08-31  0.0.0     DA       Creation 
+## -- 2024-09-04  0.1.0     DA       Updates on class design
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 0.0.0 (2024-08-31)
+Ver. 0.1.0 (2024-09-04)
 
-This module provides ...
+This module provides basic classes around the topic closed-loop control.
+
 """
 
-from mlpro.bf.streams.basics import InstDict, Instance, StreamTask, StreamWorkflow
+from mlpro.bf.various import Log
+from mlpro.bf.mt import Task, Workflow
+from mlpro.bf.math import Function
+from mlpro.bf.streams import InstDict, Instance, StreamTask, StreamWorkflow, StreamShared, StreamScenario
 from mlpro.bf.systems import Action, System
+from mlpro.bf.various import Log
 
 
 
@@ -27,6 +33,7 @@ class SetPoint (Instance):
     """
 
     pass
+
 
 
 
@@ -89,6 +96,58 @@ class Controller (StreamTask):
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
+class ControllerFct (Controller):
+    """
+    Wrapper class for controllers based on a mathematical function mapping an error to an action.
+
+    Parameters
+    ----------
+    p_fct : Function
+        Function object mapping a control error to an action
+
+    See class Controller for further parameters.
+    """
+
+    C_TYPE          = 'Controller Fct'
+    C_NAME          = ''
+
+## -------------------------------------------------------------------------------------------------
+    def __init__( self, 
+                  p_fct : Function,
+                  p_name: str = None, 
+                  p_range_max=Task.C_RANGE_THREAD, 
+                  p_duplicate_data: bool = False, 
+                  p_visualize: bool = False, 
+                  p_logging = Log.C_LOG_ALL, 
+                  **p_kwargs ):
+        
+        super().__init__( p_name = p_name, 
+                          p_range_max = p_range_max, 
+                          p_duplicate_data = p_duplicate_data, 
+                          p_visualize = p_visualize, 
+                          p_logging = p_logging, 
+                          **p_kwargs )
+
+        self._fct : Function = p_fct
+
+
+## -------------------------------------------------------------------------------------------------
+    def set_parameter(self, **p_param):
+        
+        raise NotImplementedError
+
+
+## -------------------------------------------------------------------------------------------------
+    def compute_action(self, p_ctrl_error: CTRLError) -> Action:
+
+        raise NotImplementedError
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 class MultiController (Controller, StreamWorkflow):
     """
     """
@@ -102,9 +161,88 @@ class MultiController (Controller, StreamWorkflow):
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class ControlSystem (Controller, StreamWorkflow):
+class ControlSystem (StreamTask):
     """
+    Wrapper class for state-based systems.
     """
 
     C_TYPE          = 'Control System'
     C_NAME          = ''
+
+## -------------------------------------------------------------------------------------------------
+    def __init__( self, 
+                  p_system : System,
+                  p_name: str = None, 
+                  p_range_max=Task.C_RANGE_THREAD, 
+                  p_duplicate_data: bool = False, 
+                  p_visualize: bool = False, 
+                  p_logging = Log.C_LOG_ALL, 
+                  **p_kwargs ):
+        
+        super().__init__( p_name = p_name, 
+                          p_range_max = p_range_max, 
+                          p_duplicate_data = p_duplicate_data, 
+                          p_visualize = p_visualize, 
+                          p_logging = p_logging, 
+                          **p_kwargs )
+
+        self._system : System = p_system
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class ControlShared (StreamShared):
+
+## -------------------------------------------------------------------------------------------------
+    def change_setpoint( p_setpoint : SetPoint ):
+
+        raise NotImplementedError
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class ControlCycle (StreamWorkflow):
+    """
+    Container class for all tasks of a control cycle.
+    """
+
+    C_TYPE          = 'Control Cycle'
+    C_NAME          = ''
+
+## -------------------------------------------------------------------------------------------------
+    def __init__( self, 
+                  p_name: str = None, 
+                  p_range_max = Workflow.C_RANGE_THREAD, 
+                  p_class_shared = ControlShared, 
+                  p_visualize : bool = False,
+                  p_logging = Log.C_LOG_ALL, 
+                  **p_kwargs ):
+
+        super().__init__( p_name=p_name, 
+                          p_range_max=p_range_max, 
+                          p_class_shared=p_class_shared, 
+                          p_visualize=p_visualize,
+                          p_logging=p_logging, 
+                          **p_kwargs )
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class ControlScenario ( StreamScenario ):
+    """
+    ...
+    """
+
+## -------------------------------------------------------------------------------------------------
+    def get_control_board(self) -> ControlShared:
+
+        raise NotImplementedError
