1a9d1622188fa9110740b171c5296064f4a2e867
detlefarend <detlef@arend-privat.net>
2024-09-12 15:27:56 +0200

    OA: Basics of online adaptive closed-loop control #1047

--- src/mlpro/oa/control/basics.py
+++ src/mlpro/oa/control/basics.py
@@ -5,109 +5,182 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
-## -- 2024-08-31  0.0.0     DA       Creation 
+## -- 2024-09-12  0.0.0     DA       Creation 
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 0.0.0 (2024-08-31)
+Ver. 0.0.0 (2024-09-12)
 
-This module provides ...
+This module provides basic classes around the topic online-adaptive closed-loop control.
+
 """
 
 
-# from mlpro.bf.various import *
+
+from mlpro.bf.control import Controller, MultiController, ControllerFct
+from mlpro.bf.ml import Model
 
 
 
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class Example:
+class OAController (Controller, Model):
     """
-    The __init__ method should be documented in the class level docstring and the docstring itself
-    should not go beyond 100 characters length (within the dash separator). Sections inside the 
-    docstring can be seperated like the reStructuredText format.
-    
-    Parameters are documented in the Parameters section.
-    Public attributes of classes are documented inisde Attributes section.
-    Returns attronites are documented in the Returns section.
-    
-    A few examples of data types: int / str / bool / list of str / tuple of int / float / None / dict.
-    If the parameters have default values, you should add "TYPE, optional" as part of the type
-    and "The default is ...." as part of the description.
-    
-    .. _Further_formatting_information: 
-        https://numpydoc.readthedocs.io/en/latest/format.html
-    
-    Notes
-    -----
-        The content inside the section should be indented. 
-    
+    Template class for online-adaptive closed-loop controllers.
+    """
+
+    C_TYPE          = 'OA Controller'
+    C_NAME          = '????'
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class OAControllerFct (OAController):
+    """
+    Wrapper class for controllers based on an online-adaptive function mapping an error to an action.
+
     Parameters
     ----------
-    p_arg1 : str
-        Explanation of the first parameter.
-    p_arg2 : bool
-        Explanation of the second parameter. The default is True.
-        
-    Attributes
-    ----------
-    C_MY_CONSTANT = 'My static value'
-        Explanation of the public constant C_MY_CONSTANT.
+    p_fct : Function
+        Function object mapping a control error to an action
 
+    See class Controller for further parameters.
     """
-    
-    attr1 = None
+
+    C_TYPE          = 'OA Controller Fct'
+    C_NAME          = ''
+
+## -------------------------------------------------------------------------------------------------
+    def switch_adaptivity(self, p_ada: bool):
+        try:
+            self._fct.switch_adaptivity(p_ada)
+        except:
+            pass
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class MultiController (Controller, StreamWorkflow):
+    """
+    """
+
+    C_TYPE          = 'Multi-Controller'
+    C_NAME          = ''
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class ControlSystem (StreamTask):
+    """
+    Wrapper class for state-based systems.
+    """
+
+    C_TYPE          = 'Control System'
+    C_NAME          = ''
 
 ## -------------------------------------------------------------------------------------------------
     def __init__( self, 
-                  p_arg1 : str, 
-                  p_arg2 : bool = True ):
-                  
-        self.attr1 = p_arg1
-        pass
+                  p_system : System,
+                  p_name: str = None, 
+                  p_range_max=Task.C_RANGE_THREAD, 
+                  p_duplicate_data: bool = False, 
+                  p_visualize: bool = False, 
+                  p_logging = Log.C_LOG_ALL, 
+                  **p_kwargs ):
+        
+        super().__init__( p_name = p_name, 
+                          p_range_max = p_range_max, 
+                          p_duplicate_data = p_duplicate_data, 
+                          p_visualize = p_visualize, 
+                          p_logging = p_logging, 
+                          **p_kwargs )
+
+        self._system : System = p_system
+
+
+
 
 
 ## -------------------------------------------------------------------------------------------------
-    def example_method(self, p_arg1):
+## -------------------------------------------------------------------------------------------------
+class ControlPanel (Log):
+    """
+    Enables external control of a closed-loop control.
+    """
+
+    C_TYPE          = 'Control Panel'
+    C_NAME          = '????'
+
+## -------------------------------------------------------------------------------------------------
+    def start(self):
         """
-        Example on how to document return type. 
+        (Re-)starts a closed-loop control.
+        """
         
-        Notes
-        -----
-            The name of the return value is required for better understanding 
-            of the code. The return value is parsed similarly as parameters 
-            value, meaning that multiple return value is also possible.
-        
+        self.log(Log.C_LOG_TYPE_S, 'Control process started')
+        self._start()
+
+
+## -------------------------------------------------------------------------------------------------
+    def _start(self):
+        """
+        Custom method to (re-)start a closed-loop control.
+        """
+
+        raise NotImplementedError
+    
+
+## -------------------------------------------------------------------------------------------------
+    def stop(self):
+        """
+        Ends a closed-loop control.
+        """
+
+        self.log(Log.C_LOG_TYPE_S, 'Control process stopped')
+        self._stop()
+
+
+## -------------------------------------------------------------------------------------------------
+    def _stop(self):
+        """
+        Custom method to end a closed-loop control.
+        """
+
+        raise NotImplementedError
+    
+
+## -------------------------------------------------------------------------------------------------
+    def change_setpoint( self, p_setpoint : SetPoint ):
+        """
+        Changes the setpoint values of a closed-loop control.
+
         Parameters
         ----------
-        p_arg1 : TYPE
-            explanation of the first parameter.
-                
-        Returns
-        -------
-        p_arg1: TYPE
-            Description of the returned value.
+        p_setpoint: SetPoint
+            New setpoint values.
         """
-        return p_arg1
-        
+
+        self.log(Log.C_LOG_TYPE_S, 'Setpoint values changed to', p_setpoint.values)
+        self._change_setpoint( p_setpoint = p_setpoint )
+
 
 ## -------------------------------------------------------------------------------------------------
-    def example_method_no_return(self, p_arg1):
+    def _change_setpoint( self, p_setpoint : SetPoint ):
         """
-        Example on how to document return type. 
-        
-        Notes
-        -----
-            When there is no item to be returned, the return section is omitted.
-        
-        Parameters
-        ----------
-        p_arg1 : TYPE
-            explanation of the first parameter.
-                
+        Custom method to change setpoint values.
         """
-        return 
+
+        raise NotImplementedError
 
 
 
@@ -115,22 +188,88 @@ class Example:
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class Example2:
+class ControlShared (StreamShared, ControlPanel):
+    
+## -------------------------------------------------------------------------------------------------
+    def _start(self):
+        
+        raise NotImplementedError
+    
+
+## -------------------------------------------------------------------------------------------------
+    def _stop(self):
+        
+        raise NotImplementedError
+    
+
+## -------------------------------------------------------------------------------------------------
+    def _change_setpoint(self, p_setpoint: SetPoint):
+        
+        raise NotImplementedError
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class ControlCycle (StreamWorkflow):
+    """
+    Container class for all tasks of a control cycle.
+    """
+
+    C_TYPE          = 'Control Cycle'
+    C_NAME          = ''
+
+## -------------------------------------------------------------------------------------------------
+    def __init__( self, 
+                  p_name: str = None, 
+                  p_range_max = Workflow.C_RANGE_THREAD, 
+                  p_class_shared = ControlShared, 
+                  p_visualize : bool = False,
+                  p_logging = Log.C_LOG_ALL, 
+                  **p_kwargs ):
+
+        super().__init__( p_name=p_name, 
+                          p_range_max=p_range_max, 
+                          p_class_shared=p_class_shared, 
+                          p_visualize=p_visualize,
+                          p_logging=p_logging, 
+                          **p_kwargs )
+
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class ControlScenario ( StreamScenario ):
     """
     ...
-
     """
 
 ## -------------------------------------------------------------------------------------------------
-    def __init__(self, p_arg1, p_arg2):
-        pass
+    def __init__( self, 
+                  p_mode, 
+                  p_cycle_limit=0, 
+                  p_visualize:bool=False, 
+                  p_logging=Log.C_LOG_ALL ):
 
+        self._control_cycle : ControlCycle = None
+
+        super.__init__( p_mode, 
+                        p_cycle_limit=p_cycle_limit, 
+                        p_auto_setup=True, 
+                        p_visualize=p_visualize, 
+                        p_logging=p_logging )
+        
 
 ## -------------------------------------------------------------------------------------------------
-    def example_method(self, p_arg1):
+    def get_control_panel(self) -> ControlPanel:
         """
+        Returns
+        -------
+        panel : ControlPanel
+            Object that enables the external control of a closed-loop control process.
         """
-        pass
-
-
-
+        return self._control_cycle.get_so()
