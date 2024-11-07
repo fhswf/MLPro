7d94f6f2856f0e3e8f2c3300bf8be7f25a1a07f4
detlefarend <detlef@arend-privat.net>
2024-11-07 17:15:00 +0100

    BF: Basics of closed-loop control #1046

--- src/mlpro/bf/control/operators/__init__.py
+++ src/mlpro/bf/control/operators/__init__.py
@@ -1,2 +1,3 @@
 from mlpro.bf.control.operators.comparator import Comparator
 from mlpro.bf.control.operators.integrator import Integrator
+from mlpro.bf.control.operators.converter import Converter
