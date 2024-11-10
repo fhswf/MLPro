1f12ca8b62fe303910d5e489dee7a76b9f1749a9
Amerik Singh <amerik.singh13@gmail.com>
2024-11-10 22:10:56 +0100

    BF-Systems: Pool Object for MultiSystem #767

--- src/mlpro/bf/systems/pool/__init__.py
+++ src/mlpro/bf/systems/pool/__init__.py
@@ -1,3 +1,4 @@
 from mlpro.bf.systems.pool.doublependulum import DoublePendulumSystemS4, DoublePendulumSystemS7
 from mlpro.bf.systems.pool.fox import Fox
 from mlpro.bf.systems.pool.first_order_system import PT1
+from mlpro.bf.systems.pool.second_order_system import PT2
