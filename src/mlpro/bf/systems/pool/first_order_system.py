b541e7da3fa37dfa7295fac95986165a62b8a9af
Amerik Singh <amerik.singh13@gmail.com>
2024-11-10 19:22:57 +0100

    BF: Controlled system PT1 #1084

--- src/mlpro/bf/systems/pool/first_order_system.py
+++ src/mlpro/bf/systems/pool/first_order_system.py
@@ -5,15 +5,16 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
-## -- 2024-11-05  0.1.0     ASP       Initial implementation class PT1
+## -- 2024-11-05  0.1.0     ASP      Initial implementation class PT1
+## -- 2024-11-05  0.2.0     ASP      class PT1: update methods __init__(), _setup_spaces()
+## -- 2024-11-10  0.3.0     ASP      class PT1: update methods __init__(), _setup_spaces()
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 0.2.0 (2024-11-05)
-
-This module provides a simple demo system that represent a first order system
+Ver. 0.3.0 (2024-11-10)
+This module provides a simple demo system that represent a first-order-system
+    Further information: https://www.circuitbread.com/tutorials/first-order-systems-2-2
 """
-
 
 import random
 from mlpro.bf.various import Log
@@ -29,7 +30,7 @@ from mlpro.bf.systems import State, Action, System
 ## -------------------------------------------------------------------------------------------------
 class PT1 (System):
     """
-    ...
+    Class first-order-system
     """
 
     C_NAME          = 'PT1'
@@ -44,14 +45,13 @@ class PT1 (System):
                   p_sys_num:int,
                   p_id=None, 
                   p_name = None, 
-                  p_num_dim:int= 1,
                   p_range_max = Task.C_RANGE_NONE, 
                   p_visualize = False, 
                   p_logging=Log.C_LOG_ALL, 
                   **p_kwargs ):
         
         """
-        Initialisiert ein PT1-Glied.
+        Initialsize first-order-system.
 
         Parameters
         ----------
@@ -59,12 +59,12 @@ class PT1 (System):
             Gain factor of the system.
         p_T : float
             Time constant of the system.
+        p_sys_num : float
+            Num id of the system
         """
 
         self.K = p_K          
-        self.T = p_T          
-        self._y_prev = 0.0   
-        self._previous_tstamp =None
+        self.T = p_T
         self._sys_num = p_sys_num
 
         super().__init__( p_id = p_id, 
@@ -74,18 +74,14 @@ class PT1 (System):
                           p_visualize = p_visualize, 
                           p_logging = p_logging )       
 
-        self._state_space, self._action_space = self._setup_spaces(p_num_dim=p_num_dim)
+        self._state_space, self._action_space = self._setup_spaces(p_sys_num=p_sys_num)
         
 
 ## -------------------------------------------------------------------------------------------------
-    def _setup_spaces(self, p_num_dim: int):
+    def _setup_spaces(self, p_sys_num: int):
 
-        state_action_space : MSpace = ESpace()
-        start = self._sys_num*2
-        stop = p_num_dim + self._sys_num*2
-
-        for i in range(start,stop):
-            state_action_space.add_dim( p_dim = Dimension( p_name_short = 'var ' + str(i),
+        state_action_space : MSpace = ESpace()    
+        state_action_space.add_dim( p_dim = Dimension( p_name_short = 'SYS ' + str(p_sys_num),
                                                            p_base_set = Dimension.C_BASE_SET_R,
                                                            p_boundaries = self.C_BOUNDARIES ) )
         
@@ -95,8 +91,9 @@ class PT1 (System):
   
 ## -------------------------------------------------------------------------------------------------
     def _reset(self, p_seed=None):
+
         random.seed( p_seed )
-        new_state = State( p_state_space = self.get_state_space(), p_initial = True )
+        new_state = State( p_state_space = self.get_state_space(), p_initial = True )        
         self._set_state( p_state = new_state )
 
 
@@ -110,26 +107,16 @@ class PT1 (System):
         new_state = State( p_state_space = self.get_state_space())
 
         # get control Variable
-        u= p_action.get_elem(p_id=agent_id).get_values()[0]
+        u = p_action.get_elem(p_id=agent_id).get_values()[0]
 
         # get current state
-        self._y_prev = p_state.values[0]
+        y_prev = p_state.values[0]
 
-        dt =0#p_step    
-
-        #get timestamp
-        tstamp=p_action.get_tstamp()
-
-        if self._previous_tstamp is not None:
-            dt = tstamp- self._previous_tstamp
-            dt = dt.total_seconds()
-
+        dt = p_step 
+        
         # rekursions function of first oder system
-        y = (dt* self.K * u + self.T * self._y_prev) / (self.T + dt)  
-
-        #save current timestamp
-        self._previous_tstamp= tstamp
-
+        y = (dt* self.K * u + self.T * y_prev) / (self.T + dt) 
+        
         #set values of new state state 
         new_state.values = [y]    
 
