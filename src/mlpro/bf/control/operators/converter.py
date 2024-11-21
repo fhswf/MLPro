2a6a03f8936905c894cd6d308ee1edcdf14fb1eb
Amerik Singh <amerik.singh13@gmail.com>
2024-11-21 21:14:36 +0100

    BF: Basics of closed-loop control #1046

--- src/mlpro/bf/control/operators/converter.py
+++ src/mlpro/bf/control/operators/converter.py
@@ -1,26 +1,6 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- Project : MLPro - The integrative middleware framework for standardized machine learning
 ## -- Package : mlpro.bf.control.operators
-<<<<<<< HEAD
-## -- Module  : converter.py
-## -------------------------------------------------------------------------------------------------
-## -- History :
-## -- yyyy-mm-dd  Ver.      Auth.    Description
-## -- 2024-11-04  0.1.0     ASP      Creation and initial implementation
-## -------------------------------------------------------------------------------------------------
-
-"""
-Ver. 0.4.0 (2024-10-13)
-
-This module provides an implementation of a comparator that determins the control error based on 
-setpoint and controlled variable (system state).
-
-"""
-from mlpro.bf.various import Log
-from mlpro.bf.mt import Task
-from mlpro.bf.streams import InstDict, InstTypeNew
-from mlpro.bf.control import Operator,ControlData,ControlledVariable
-=======
 ## -- Module  : integrator.py
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
@@ -44,7 +24,6 @@ from mlpro.bf.mt import Log, Task
 from mlpro.bf.streams import InstDict, InstTypeNew
 from mlpro.bf.control import ControlData, Operator, get_ctrl_data
 
->>>>>>> origin/bf/oa/control
 
 
 
@@ -52,18 +31,6 @@ from mlpro.bf.control import ControlData, Operator, get_ctrl_data
 ## -------------------------------------------------------------------------------------------------
 class Converter (Operator):
     """
-<<<<<<< HEAD
-    The converter converts the source type into the destination type.
-    It consumes (not to say: removes) the current source instance variable and replaces it by destination instance variable.
-    """
-
-    C_NAME      = 'Converter'
-
-
-    def __init__(self,
-                 p_src_type: ControlData,
-                 p_dst_type:ControlData,
-=======
     The converter converts the source type into the destination type. It consumes (not to say: removes) 
     the current source instance variable and replaces it by destination instance variable.
 
@@ -81,46 +48,17 @@ class Converter (Operator):
     def __init__(self,
                  p_src_type: type,
                  p_dst_type: type,
->>>>>>> origin/bf/oa/control
                  p_range_max=Task.C_RANGE_THREAD, 
                  p_visualize=False, 
                  p_logging=Log.C_LOG_ALL,
                  **p_kwargs):
-<<<<<<< HEAD
-=======
         
->>>>>>> origin/bf/oa/control
         super().__init__(p_range_max, p_visualize, p_logging, **p_kwargs)
 
         self._src_type = p_src_type
         self._dst_type = p_dst_type
         self._duplicate_data = True
 
-<<<<<<< HEAD
-## -------------------------------------------------------------------------------------------------
-    def _run(self, p_inst: InstDict):
-
-        remove = True if self._src_type !=ControlledVariable else False
-        
-        # 1 Get source instance 
-        src_instance:ControlData = self._get_instance( p_inst = p_inst, p_type = self._src_type, p_remove = remove )
-
-        if src_instance is None: 
-            self.log(Log.C_LOG_TYPE_E, f'{self._src_type} missing!')
-            return
-        
-        # 2 Create destination instance 
-        dst_instance =self._dst_type(p_id = self.get_so().get_next_inst_id(),
-                             p_value_space = src_instance.value_space, 
-                             p_values = src_instance.values,
-                             p_tstamp = self.get_so().get_tstamp() )
-
-
-        # 3 Store destination instance      
-        p_inst[dst_instance.id] = (InstTypeNew, dst_instance)
-
-
-=======
 
 ## -------------------------------------------------------------------------------------------------
     def _run(self, p_inst: InstDict):
@@ -143,4 +81,3 @@ class Converter (Operator):
 
         # 3 Store destination instance      
         p_inst[dst_instance.id] = (InstTypeNew, dst_instance)
->>>>>>> origin/bf/oa/control
