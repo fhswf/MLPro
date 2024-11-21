886b4916d7072064ddac7b4558193fcbbe1fdab8
Amerik Singh <amerik.singh13@gmail.com>
2024-11-21 21:16:28 +0100

    RTD: Closed-loop control BF #1080

--- src/mlpro/bf/control/controlsystems/__init__.py
+++ src/mlpro/bf/control/controlsystems/__init__.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-from mlpro.bf.control.controlsystems.basic import ControlSystemBasic, CascadeControlSystem
-=======
 from mlpro.bf.control.controlsystems.cascade import ControllerList, ControlledSystemList, CascadeControlSystem
 from mlpro.bf.control.controlsystems.basic import BasicControlSystem
->>>>>>> origin/bf/oa/control
