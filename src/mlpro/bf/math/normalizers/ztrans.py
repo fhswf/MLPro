Bug: OA.NormalizerZTrans loses internal normalization parameters #1111

src/mlpro/bf/math/normalizers/ztrans.py
@@ -86,9 +86,10 @@ class NormalizerZTrans (Normalizer, ScientificObject):
 
         # 1 Update on dataset
         if p_dataset is not None:
-            self._std   = np.std(p_dataset, axis=0, dtype=np.float64)
-            self._mean  = np.mean(p_dataset, axis=0, dtype=np.float64)
             self._n     = len(p_dataset)
+            self._mean  = np.mean(p_dataset, axis=0, dtype=np.float64)
+            self._std   = np.std(p_dataset, axis=0, dtype=np.float64)
+            self._s     = np.square(self._std) * self._n
 
             if self._param_new is None: 
                 self._param_new = np.zeros([2, self._std.shape[-1]])
@@ -110,13 +111,8 @@ class NormalizerZTrans (Normalizer, ScientificObject):
                 else:
                     self._n   += 1
                     old_mean   = self._mean.copy()
-
                     self._mean = old_mean + ( data_new - old_mean ) / self._n 
-                    try:
-                        self._s = self._s + (data_new - self._mean) * (data_new - old_mean)
-                    except:
-                        self._s = np.square(self._std) * self._n
-
+                    self._s = self._s + (data_new - self._mean) * (data_new - old_mean)
                     self._std  = np.sqrt( self._s / self._n ) 
 
                 if self._param_new is None: 
@@ -134,13 +130,8 @@ class NormalizerZTrans (Normalizer, ScientificObject):
                 if self._n > 0:
                     self._n   -= 1
                     old_mean   = self._mean.copy()
-
                     self._mean = old_mean - ( data_del - old_mean ) / self._n 
-                    try:
-                        self._s = self._s - (data_del - self._mean) * (data_del - old_mean)
-                    except:
-                        self._s = np.square(self._std) * self._n                    
-                        
+                    self._s = self._s - (data_del - self._mean) * (data_del - old_mean)
                     self._std  = np.sqrt( self._s / self._n ) 
                 
                 else: