5d66fb81eb649f4045033b3252bd34bda0936819
Amerik Singh <amerik.singh13@gmail.com>
2025-01-06 23:18:19 +0100

    BF: Controlled system PT2 #1085

--- test/howtos/bf/control/howto_bf_control_103_pid_controller_cascaded.py
+++ test/howtos/bf/control/howto_bf_control_103_pid_controller_cascaded.py
@@ -6,18 +6,19 @@
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
 ## -- 2024-09-01  0.0.0     DA       Creation
-## -- 2024-10-31  0.1.0     AS       Implementation Cascade Routine
-## -- 2024-11-10  1.0.0     AS       Refactor Cascade Routine
+## -- 2024-10-31  0.1.0     ASP       Implementation Cascade Routine
+## -- 2024-11-10  1.0.0     ASP       Refactor Cascade Routine
 ##                                      - Added HowTo description 
 ##                                      - Added PT2 Controlled System
 ##                                      - Changed paramters for the controlled system PT1
 ##                                      - Removed CasscadedSystem 
 ##                                      - adjust values of controlled systems properties and PID properties 
-## -- 2024-12-03  1.1.0     AS       Update PT1 und PT2                    
+## -- 2024-12-03  1.1.0     ASP       Update PT1 und PT2  
+## -- 2025-01-06  1.2.0     ASP       Update whole HowTo                         
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 1.0.0 (2024-11-10)
+Ver. 1.3.0 (2025-01-06)
 
 This HowTo is intended to simulate the temperature control of a stirred vessel. 
 The cascade control consists of two cascades, the inner cascade consists of a P-controller and a first-order system, 
@@ -44,102 +45,112 @@ from mlpro.bf.control.controllers.pid_controller import PIDController
 
 
 
-# 1 Preparation of demo/unit test mode
-if __name__ == '__main__':
-    # 1.1 Parameters for demo mode
-    cycle_limit = 500
-    num_dim     = 1
+# 2 Prepare for test
+if __name__ == "__main__":
     logging     = Log.C_LOG_ALL
     visualize   = True
-    step_rate   = 2
-  
+    step_rate   = 1
+    num_dim     = 1
 else:
-    # 1.2 Parameters for internal unit test
     cycle_limit = 5
-    num_dim     = 4
     logging     = Log.C_LOG_NOTHING
     visualize   = False
     step_rate   = 1
-
-setpoint_value = 400
-
+    num_dim     = 1
 
 
-# 2 Setup the control system
-# 2.1 Controller and controlled system of the outer cascade
-my_ctrl_sys_1 = PT1(p_K=25,
-                p_T=1200,
-                p_sys_num=0,
-                p_y_start=0,
-                p_latency = timedelta( seconds = 0.1 ),
+# 2.1 Define init parameters and calculate cycle limit
+
+#init controlled systems parameter
+pt2_K = 1
+pt2_D = 1.6165
+pt2_w_0 = 0.00577
+pt1_T = 1200
+pt1_K = 25
+
+# calculate cycle limit
+simulation_time = 500 *1 / pt2_w_0
+cycle_limit = int(simulation_time / 2)
+
+# init setpoint
+setpoint_value = 40
+
+
+# 3 Setup inner casscade
+
+# 3.1 controlled system 
+my_ctrl_sys_1 = PT1(p_K = pt1_T,
+                p_T = pt1_K,
+                p_sys_num = 0,
+                p_y_start = 0,
+                p_latency = timedelta( seconds = 1 ),
                 p_visualize = visualize,
                 p_logging = logging )
 
-
-my_ctrl_sys_1.reset( p_seed = 1 )
-
-# 2.2 Controller
-my_ctrl_1 = PIDController( p_input_space = my_ctrl_sys_1.get_state_space(),
-                       p_output_space = my_ctrl_sys_1.get_action_space(),
-                       p_Kp=9.43,
-                       p_Tn=228,
-                       p_Tv=50,
-                       p_name = 'PID Controller',
-                       p_visualize = visualize,
-                       p_logging = logging )
+my_ctrl_sys_1.reset( p_seed = 42 )   
 
 
-# 2.2 Controller and controlled system of the inner cascade
-my_ctrl_sys_2 = PT2(p_K=1,
-                    p_D=1.6165,
-                    p_omega_0=0.00577,
-                    p_sys_num=1,
-                    p_max_cycle=cycle_limit,
-                    p_latency = timedelta( seconds = 0.1 ),
+# 3.2 P-Controller
+my_ctrl_2 = PIDController( p_input_space = my_ctrl_sys_1.get_state_space(),
+                    p_output_space = my_ctrl_sys_1.get_action_space(),
+                    p_Kp = 0.36,
+                    p_Tn = 0,
+                    p_Tv = 0,
+                    p_integral_off = True,
+                    p_derivitave_off = True,
+                    p_name = 'PID Controller2',
                     p_visualize = visualize,
                     p_logging = logging )
 
 
-my_ctrl_sys_2.reset( p_seed = 2 )
+# 4 Setup outer casscade
 
-# 2.2 P-Controller
-my_ctrl_2 = PIDController( p_input_space = my_ctrl_sys_2.get_state_space(),
-                       p_output_space = my_ctrl_sys_2.get_action_space(),
-                       p_Kp=0.36,
-                       p_Tn=0,
-                       p_Tv=0,
-                       p_integral_off=True,
-                       p_derivitave_off=True,
-                       p_name = 'PID Controller2',
-                       p_visualize = visualize,
-                       p_logging = logging )
+# 4.1 controlled system 
+my_ctrl_sys_2 = PT2(p_K = pt2_K,
+                    p_D = pt2_D,
+                    p_omega_0 = pt2_w_0,
+                    p_sys_num = 1,
+                    p_max_cycle = cycle_limit,
+                    p_latency = timedelta( seconds = 4 ),
+                    p_visualize = visualize,
+                    p_logging = logging )
+
+my_ctrl_sys_2.reset( p_seed = 42 )
+
+#4.2 Init PID-Controller
+my_ctrl_1 = PIDController( p_input_space = my_ctrl_sys_2.get_state_space(),
+                    p_output_space = my_ctrl_sys_2.get_action_space(),
+                    p_Kp = 9.43,
+                    p_Tn = 228,
+                    p_Tv = 50,
+                    p_name = 'PID Controller',
+                    p_visualize = visualize,
+                    p_logging = logging )  
 
 
-# 2.3 Cascade control system
+# 5. Cascaded control system
 mycontrolsystem = CascadeControlSystem( p_mode = Mode.C_MODE_SIM,
-                                        p_controllers = [ my_ctrl_1, my_ctrl_2 ],
-                                        p_controlled_systems = [ my_ctrl_sys_1, my_ctrl_sys_2 ],
+                                        p_controllers = [ my_ctrl_1, my_ctrl_2],
+                                        p_controlled_systems = [my_ctrl_sys_2, my_ctrl_sys_1 ],
                                         p_name = 'Stirring vessel',
                                         p_cycle_limit = cycle_limit,
                                         p_visualize = visualize,
                                         p_logging = logging )
 
 
-
-# 3 Set initial setpoint values for all control workflows (=cascades) of the control system
+# 6 Set initial setpoint values for all control workflows (=cascades) of the control system
 for panel_entry in mycontrolsystem.get_control_panels():
-    panel_entry[0].set_setpoint( p_values = np.ones(shape=(num_dim))* setpoint_value )
+    panel_entry[0].set_setpoint( p_values = np.ones(shape = (num_dim)) * setpoint_value )
 
 
-
-# 4 Run some control cycles
-if ( __name__ == '__main__' ) and visualize:
-    mycontrolsystem.init_plot( p_plot_settings=PlotSettings( p_view = PlotSettings.C_VIEW_ND,
-                                                             p_view_autoselect = True,
-                                                             p_step_rate = step_rate,
-                                                             p_plot_horizon = 100 ) )
-    input('\nPlease arrange all windows and press ENTER to start control processing...')
-
+# 7 Run some control cycles
+if visualize:
+    mycontrolsystem.init_plot( p_plot_settings = PlotSettings( p_view = PlotSettings.C_VIEW_ND,
+                                                            p_view_autoselect = True,
+                                                            p_step_rate = step_rate,
+                                                            p_plot_horizon = 100 ) )
+input('\nPlease arrange all windows and press ENTER to start control processing...')    
+    
 mycontrolsystem.run()
 
 if __name__ == '__main__':
--- test/howtos/bf/control/howto_bf_control_PT_102_pid_controller_PT2.py
+++ test/howtos/bf/control/howto_bf_control_PT_102_pid_controller_PT2.py
@@ -5,19 +5,20 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
-## -- 2024-11-11  0.1.0     AS       Creation
-## -- 2024-12-03  0.1.0     AS       Update PID Parameter
+## -- 2024-11-11  0.1.0     ASP       Creation
+## -- 2024-12-03  0.2.0     ASP       Update PID Parameter
+## -- 2025-01-06  0.3.0     ASP       Update whole HowTo
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 0.1.0 (2024-12-03)
+Ver. 0.3.0 (2025-01-06)
 
 This howto demonstrates a basic control system consisting of a PID
-controller and a first-order-system (PT1) as a controlled system.
+controller and a second-order-system (PT2) as a controlled system.
 
 You will learn:
 
-1) How to set up a basic control system with a PT1-controlled system and PID-Controller
+1) How to set up a basic control system with a PT2-controlled system and PID-Controller
 
 2) How to execute a control system and to change the setpoint from outside
 
@@ -38,60 +39,63 @@ from mlpro.bf.control.controlsystems import BasicControlSystem
 
 
 
-# 1 Preparation of demo/unit test mode
-if __name__ == '__main__':
-    # 1.1 Parameters for demo mode
-    cycle_limit = 500
-    num_dim     = 1
+# 1 Prepare for test
+if __name__ == "__main__":
     logging     = Log.C_LOG_ALL
     visualize   = True
-    step_rate   = 2
-  
+    step_rate   = 1
+    num_dim     = 1
 else:
-    # 1.2 Parameters for internal unit test
     cycle_limit = 5
-    num_dim     = 4
     logging     = Log.C_LOG_NOTHING
     visualize   = False
     step_rate   = 1
+    num_dim     = 1
 
-if __name__ == '__main__':
-    i = input(f'\n\nDimensionality (press ENTER for {num_dim}): ')
-    if i != '': num_dim = int(i)
 
-    
-    i = input(f'\n\nStep rate (visualization) (press ENTER for {step_rate}): ')
-    if i != '': step_rate = int(i)
+# 1.1 Define init parameters and calculate cycle limit
 
+#init controlled systems parameter
+pt2_K = 5
+pt2_D = 0.5
+pt2_w_0 = 5
+
+# calculate cycle limit
+cycle_time = 1 / (20 * pt2_w_0)
+simulation_time = 15 *1 / pt2_w_0
+cycle_limit = int(simulation_time / cycle_time)
+
+# init setpoint
 setpoint_value = 10
+
 
 # 2 Setup the control system
 
 # 2.1 Controlled system
-my_ctrl_sys = PT2(p_K=1,
-                    p_D=1,
-                    p_omega_0=5,
-                    p_sys_num=1,
-                    p_max_cycle=cycle_limit,
-                    p_latency = timedelta( seconds = 0.01 ),
+my_ctrl_sys = PT2(p_K = pt2_K,
+                    p_D = pt2_D,
+                    p_omega_0 = pt2_w_0,
+                    p_sys_num = 1,
+                    p_max_cycle = cycle_limit,
+                    p_latency = timedelta( seconds = cycle_time ),
                     p_visualize = visualize,
                     p_logging = logging )
 
-
-
 my_ctrl_sys.reset( p_seed = 1 )
+
 
 # 2.2 Controller
 my_ctrl = PIDController( p_input_space = my_ctrl_sys.get_state_space(),
                        p_output_space = my_ctrl_sys.get_action_space(),
-                       p_Kp=0.59,
-                       p_Tn=1,
-                       p_Tv=0.5,
-                       p_integral_off=False,
-                       p_derivitave_off=False,
+                       p_Kp = 0.59,
+                       p_Tn = 0.5,
+                       p_Tv = 0,
+                       p_integral_off = False,
+                       p_derivitave_off = True,
                        p_name = 'PID Controller',
                        p_visualize = visualize,
                        p_logging = logging )
+
 
 # 2.3 Basic control system
 mycontrolsystem = BasicControlSystem( p_mode = Mode.C_MODE_SIM,
@@ -104,16 +108,14 @@ mycontrolsystem = BasicControlSystem( p_mode = Mode.C_MODE_SIM,
                                       p_logging = logging )
 
 
-
 # 3 Set initial setpoint values and reset the controlled system
-mycontrolsystem.get_control_panels()[0][0].set_setpoint( p_values = np.ones(shape=(num_dim))*setpoint_value)
+mycontrolsystem.get_control_panels()[0][0].set_setpoint( p_values = np.ones(shape=(num_dim)) * setpoint_value )
 my_ctrl_sys.reset( p_seed = 1 )
-
 
 
 # 4 Run some control cycles
 if ( __name__ == '__main__' ) and visualize:
-    mycontrolsystem.init_plot( p_plot_settings=PlotSettings( p_view = PlotSettings.C_VIEW_ND,
+    mycontrolsystem.init_plot( p_plot_settings = PlotSettings( p_view = PlotSettings.C_VIEW_ND,
                                                              p_view_autoselect = True,
                                                              p_step_rate = step_rate,
                                                              p_plot_horizon = 100 ) )
