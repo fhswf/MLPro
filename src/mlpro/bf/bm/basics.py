2ed94f94f6539fa450207d98bf5382367a3fde7e
detlefarend <detlef@arend-privat.net>
2025-01-28 16:59:26 +0100

    BF: Benchmark systematics #1123

---
+++ src/mlpro/bf/bm/basics.py
@@ -0,0 +1,30 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - The integrative middleware framework for standardized machine learning
+## -- Package : mlpro.bf.bm
+## -- Module  : basics.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2025-01-28  0.0.0     DA       Creation 
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 0.0.0 (2025-01-28)
+
+This module provides ...
+"""
+
+
+from mlpro.bf.ops import ScenarioBase
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class BMScenario (ScenarioBase):
+    """
+    Template class for benchmark scenarios.
+    """
+
+    C_TYPE      = 'Benchmark Scenario'
