b73bb18569b15fad4ae5bb96a70bac42c38d4d3a
detlefarend <detlef@arend-privat.net>
2025-02-24 21:12:05 +0100

    Refact: Separation of anomaly detection from drift detection #1133

--- src/mlpro/oa/streams/tasks/anomalydetectors/anomalies/__init__.py
+++ src/mlpro/oa/streams/tasks/anomalydetectors/anomalies/__init__.py
@@ -1,5 +1,3 @@
 from mlpro.oa.streams.tasks.anomalydetectors.anomalies.basics import Anomaly
-from mlpro.oa.streams.tasks.anomalydetectors.anomalies.point import PointAnomaly
-from mlpro.oa.streams.tasks.anomalydetectors.anomalies.group import GroupAnomaly
-from mlpro.oa.streams.tasks.anomalydetectors.anomalies.contextual import ContextualAnomaly
+from mlpro.oa.streams.tasks.anomalydetectors.anomalies.instancebased import *
 from mlpro.oa.streams.tasks.anomalydetectors.anomalies.clusterbased import *
