23830ad5456267858910742dc9c7b3efe896a09d
detlefarend <detlef@arend-privat.net>
2025-03-12 19:22:40 +0100

    OA: New cluster type 'ClusterBody' #1131

--- src/mlpro/oa/streams/tasks/clusteranalyzers/clusters/properties/body.py
+++ src/mlpro/oa/streams/tasks/clusteranalyzers/clusters/properties/body.py
@@ -1,25 +1,17 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- Project : MLPro - The integrative middleware framework for standardized machine learning
 ## -- Package : mlpro.oa.streams.tasks.clusteranalyzers.clusters.properties
-## -- Module  : centroid.py
+## -- Module  : body.py
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
-## -- 2024-05-04  0.1.0     DA       Creation
-## -- 2024-05-29  0.2.0     DA       Refactoring
-## -- 2024-05-30  0.3.0     DA       Global aliases: new boolean param ValuePrev
-## -- 2024-05-31  0.4.0     DA       Improved the stability of the plot methods
-## -- 2024-06-13  0.5.0     DA       New property definitions cprop_centroid_prev*
-## -- 2024-06-26  0.6.0     DA       Refactoring
-## -- 2024-07-13  0.7.0     DA       Refactoring
-## -- 2024-10-31  0.8.0     DA       New parent class Crosshair
-## -- 2024-12-11  0.8.1     DA       Pseudo classes if matplotlib is not installed
+## -- 2025-03-12  0.1.0     DA       Creation
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 0.8.1 (2024-12-11)
+Ver. 0.1.0 (2025-03-12)
 
-This module provides ...
+This module provides a template class for the cluster property 'body'.
 
 """
 
@@ -40,22 +32,17 @@ from mlpro.bf.streams import *
 from mlpro.bf.various import Id
 #from mlpro.bf.math.geometry import Crosshair
 from mlpro.bf.math.properties import *
-from mlpro.oa.streams.tasks.clusteranalyzers.clusters import Cluster
+from mlpro.bf.math.geometry import cprop_size_geo
+from mlpro.oa.streams.tasks.clusteranalyzers.clusters.properties import cprop_center_geo
 
 
 
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class Body (MultiProperty, Id): 
+class Body (MultiProperty): 
     """
-    This managed property provides full centroid functionality including 
-    - managing its position
-    - optionally its velocity and acceleration as auto-derivatives
-    - plot functionality
-    - renormalization
-
-    Hint: please assign the id of the cluster to the centroid as well to get a proper visualization.
+    ...
 
     Parameters
     ----------
@@ -71,7 +58,8 @@ class Body (MultiProperty, Id):
         Keyword parameters.
     """
 
-    C_PROPERTIES : PropertyDefinitions = []
+    C_PROPERTIES : PropertyDefinitions = [ cprop_center_geo,
+                                           cprop_size_geo ]
 
     C_PLOT_ACTIVE                      = True
     C_PLOT_STANDALONE                  = False
@@ -105,16 +93,5 @@ class Body (MultiProperty, Id):
 
 
 
-# Centroid with 0,1,2 order derivatives and plot functionality with/without storing previous values
-cprop_centroid       : PropertyDefinition = ( 'centroid', 0, False, Centroid )
-cprop_centroid1      : PropertyDefinition = ( 'centroid', 1, False, Centroid )
-cprop_centroid2      : PropertyDefinition = ( 'centroid', 2, False, Centroid )
-
-cprop_centroid_prev  : PropertyDefinition = ( 'centroid', 0, True, Centroid )
-cprop_centroid_prev1 : PropertyDefinition = ( 'centroid', 1, True, Centroid )
-cprop_centroid_prev2 : PropertyDefinition = ( 'centroid', 2, True, Centroid )
-
-# Geometric center with 0,1,2 order derivatives and plot functionality
-cprop_center_geo     : PropertyDefinition = ( 'center_geo', 0, False, Centroid )
-cprop_center_geo1    : PropertyDefinition = ( 'center_geo', 1, False, Centroid )
-cprop_center_geo2    : PropertyDefinition = ( 'center_geo', 2, False, Centroid )
+# Property definitions for cluster bodies
+cprop_body  : PropertyDefinition = ( 'body', 0, False, Body )
