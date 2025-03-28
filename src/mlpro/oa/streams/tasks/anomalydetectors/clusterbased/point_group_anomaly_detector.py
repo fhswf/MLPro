f4e88175f4d9144a4b26f691761e8deb85c2ac96
Devindi97 <devindi.1207@gmail.com>
2025-03-28 10:23:58 +0100

    OA: New cluster-based point and group anomaly detector #1160

---
+++ src/mlpro/oa/streams/tasks/anomalydetectors/clusterbased/point_group_anomaly_detector.py
@@ -0,0 +1,79 @@
+## -- ----------------------------------------------------------------------------------------------
+## -- Project : MLPro - The integrative middleware framework for standardized machine learning
+## -- Package : mlpro.oa.tasks.anomalydetectors.cb_detectors
+## -- Module  : point_group_anomaly_detector.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2025-03-28  0.0.0     DS       Creation
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 0.0.0 (2025-03-28)
+
+This module provides cluster based point and group anomaly detector algorithm.
+"""
+
+from mlpro.bf.various import Log
+from mlpro.bf.math.properties import *
+from mlpro.bf.streams import InstDict, InstTypeNew
+from mlpro.oa.streams import OAStreamTask
+from mlpro.oa.streams.tasks.clusteranalyzers import ClusterAnalyzer, Cluster
+from mlpro.oa.streams.tasks.anomalydetectors.clusterbased.generic import AnomalyDetectorCBGeneric
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class AnomalyDetectorCBPAGA(AnomalyDetectorCBGeneric):
+    """
+    Implementation of a cluster-based detector for point and group anomalies.
+
+    Parameters
+    ----------
+    ...
+    p_property : PropertyDefinition
+        Cluster property to be observed.
+    p_cls_anomaly : type
+        Type of anomaly events to be raised.
+    ...
+    """
+
+    C_NAME = 'Point and group'
+
+## -------------------------------------------------------------------------------------------------
+    def __init__( self,
+                  p_clusterer : ClusterAnalyzer,
+                  p_property : PropertyDefinition,
+                  p_group_anomaly_det : bool = True,
+                  p_cls_point_anomaly : type = PointAnomaly,
+                  p_cls_spatial_group_anomaly : type = SpatialGroupAnomaly,
+                  p_cls_temporal_group_anomaly : type = TemporalGroupAnomaly,
+                  p_name : str = None,
+                  p_range_max = OAStreamTask.C_RANGE_THREAD,
+                  p_ada : bool = True,
+                  p_duplicate_data : bool = False,
+                  p_visualize : bool = False,
+                  p_logging=Log.C_LOG_ALL,
+                  p_anomaly_buffer_size : int = 100,
+                  **p_kwargs ):
+        
+        self._cls_group_anomaly = p_cls_group_anomaly
+        self._cls_spatial_group_anomaly = p_cls_spatial_group_anomaly
+        self._cls_temporal_group_anomaly = p_cls_temporal_group_anomaly
+        self._group_anomaly_det = p_group_anomaly_det
+
+        super().__init__( p_clusterer=p_clusterer,
+                          p_property = p_property,
+                          p_cls_anomaly = p_cls_point_anomaly,
+                          p_name = p_name,
+                          p_range_max = p_range_max,
+                          p_ada = p_ada,
+                          p_duplicate_data = p_duplicate_data,
+                          p_visualize = p_visualize,
+                          p_logging= p_logging,
+                          p_anomaly_buffer_size = p_anomaly_buffer_size,
+                          **p_kwargs )
+
+## -------------------------------------------------------------------------------------------------
+def _run(self, p_inst: InstDict):
+    pass
