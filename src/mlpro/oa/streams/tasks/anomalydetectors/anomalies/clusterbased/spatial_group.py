d548b43c11d2476301c211f5c9f2b9a4a2c6f6d4
Devindi97 <devindi.1207@gmail.com>
2025-03-31 13:34:30 +0200

    OA: New cluster-based detector for point and group anomalies #1172

---
+++ src/mlpro/oa/streams/tasks/anomalydetectors/anomalies/clusterbased/spatial_group.py
@@ -0,0 +1,60 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - The integrative middleware framework for standardized machine learning
+## -- Package : mlpro.oa.tasks.anomalydetectors.anomalies.clusterbased
+## -- Module  : spatial_group.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2025-03-31  0.0.0     DS       Creation
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 0.0.0 (2025-03-31)
+
+This module provides a class for spatial group anomalies to be used in anomaly detection algorithms.
+"""
+
+from datetime import datetime
+
+try:
+    from matplotlib.figure import Figure
+    from matplotlib.text import Text
+    from matplotlib import patches
+except:
+    class Figure : pass
+    class Text : pass
+    class patches : pass
+    
+from mlpro.bf.plot import PlotSettings
+from mlpro.oa.streams.tasks.anomalydetectors.anomalies.clusterbased.basics import AnomalyCB
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class SpatialGroupAnomaly (AnomalyCB):
+    """
+    Event class for anomaly events when spatial group anomalies are detected.
+    
+    Parameters
+    ----------
+    p_instances : Instance
+        List of instances. Default value = None.
+    p_id : int
+        Anomaly ID. Default value = 0.
+    p_tstamp : datetime = None
+        Time of occurance of anomaly. Default = None.
+    p_visualize : bool
+        Boolean switch for visualisation. Default = False.
+    p_raising_object : object
+        Reference of the object raised. Default = None.
+    p_mean : float
+        The mean value of the anomaly. Default = None.
+    p_mean_deviation : float
+        The mean deviation of the anomaly. Default = None.
+    **p_kwargs
+        Further optional keyword arguments.
+    """
+
+    pass
+
--- src/mlpro/oa/streams/tasks/anomalydetectors/clusterbased/point_group_anomaly_detector.py
+++ src/mlpro/oa/streams/tasks/anomalydetectors/clusterbased/point_group_anomaly_detector.py
@@ -19,7 +19,9 @@ from mlpro.bf.math.properties import *
 from mlpro.bf.streams import InstDict, InstTypeNew
 from mlpro.oa.streams import OAStreamTask
 from mlpro.oa.streams.tasks.clusteranalyzers import ClusterAnalyzer, Cluster
+from mlpro.oa.streams.tasks.clusteranalyzers.clusters.properties import cprop_size
 from mlpro.oa.streams.tasks.anomalydetectors.clusterbased.basics import AnomalyDetectorCB
+from mlpro.oa.streams.tasks.anomalydetectors.anomalies.clusterbased import AnomalyCB, PointAnomaly, GroupAnomaly, SpatialGroupAnomaly, TemporalGroupAnomaly
 
 
 ## -------------------------------------------------------------------------------------------------
@@ -58,6 +60,7 @@ class AnomalyDetectorCBPAGA(AnomalyDetectorCB):
                   p_anomaly_buffer_size : int = 100,
                   **p_kwargs ):
         
+        self._cls_point_anomaly = p_cls_point_anomaly
         self._cls_group_anomaly = p_cls_group_anomaly
         self._cls_spatial_group_anomaly = p_cls_spatial_group_anomaly
         self._cls_temporal_group_anomaly = p_cls_temporal_group_anomaly
@@ -73,6 +76,43 @@ class AnomalyDetectorCBPAGA(AnomalyDetectorCB):
                           **p_kwargs)
 
 
-## -------------------------------------------------------------------------------------------------
-def _run(self, p_inst: InstDict):
-    pass
+## -------------------------------------------------------------------------------------------------    
+def _run(self, 
+         p_inst: InstDict, 
+         p_cluster : Cluster, 
+         p_property : Property):
+
+    # 1 Get all the clusters from the clusterer
+    clusters = self._clusterer.get_clusters()
+
+    # 2 Get the cluster property to be observed
+    cluster_size : Property = getattr(p_cluster, p_property)
+
+    # 3 Get average cluster size
+    avg_cluster_size = sum([getattr(c, cprop_size)[0] for c in clusters]) / len(clusters)
+
+    # 4 Calculater the threshold for the cluster size
+    thres_size = avg_cluster_size * 0.05 
+
+    # 5 Check for the  anomaly clusters
+    if 1 <= cluster_size <= thres_size:
+        # 5.1 Create a new spatial group anomaly 
+        t_stamp = datetime.now()
+        spatial_group_anomaly = self._cls_spatial_group_anomaly( p_clusters = {p_cluster.id : p_cluster},
+                                                                 p_tstamp = t_stamp,
+                                                                 p_visualize = self.get_visualize,
+                                                                 p_raising_object = self)
+        return spatial_group_anomaly
+
+    elif cluster_size == 1:
+        # 5.2 Create a new point anomaly 
+        t_stamp = datetime.now()
+        point_anomaly = self._cls_point_anomaly( p_clusters = {p_cluster.id : p_cluster},
+                                                     p_tstamp = t_stamp,
+                                                     p_visualize = self.get_visualize,
+                                                     p_raising_object = self)
+        return point_anomaly
+
+
+
+    
