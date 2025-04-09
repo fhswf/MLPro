1f306f60d30d7de37a46fd62638a7408969aa43a
steveyuwono <steveyuwono@gmail.com>
2025-04-09 15:46:47 +0200

    SbPG #982

--- src/mlpro/gt/pool/policies/sbpg.py
+++ src/mlpro/gt/pool/policies/sbpg.py
@@ -14,6 +14,10 @@ Ver. 1.0.0 (2025-04-09)
 
 This module implements a state-based potential games (SbPG) with best response and two types of 
 gradient-based learning for dynamic games.
+
+Here are the related papers for citations:
+    - Best response learning (DOI: 10.1109/TCYB.2020.3006620)
+    - Gradient-based learning (DOI: 10.1109/IECON55916.2024.10905619)
 """
 
 from mlpro.rl.models import *
@@ -70,6 +74,14 @@ class SbPG(Policy):
             p_logging=p_logging
             )
         
+        self.C_SCIREF_TYPE      = self.C_SCIREF_TYPE_INPROCEEDINGS
+        self.C_SCIREF_AUTHOR    = "Steve Yuwono, Marlon Löppenberg, Dorothea Schwung, Andreas Schwung"
+        self.C_SCIREF_BOOKTITLE = "IECON 2024 - 50th Annual Conference of the IEEE Industrial Electronics Society"
+        self.C_SCIREF_TITLE     = "Gradient-based Learning in State-based Potential Games for Self-Learning Production Systems"
+        self.C_SCIREF_YEAR      = "2024"
+        self.C_SCIREF_PAGES     = "1-7"
+        self.C_SCIREF_DOI       = "10.1109/IECON55916.2024.10905619"
+
         self._hyperparam_space  = HyperParamSpace()
         self._hyperparam_tuple  = None
         self._init_hyperparam()
@@ -98,36 +110,35 @@ class SbPG(Policy):
         num_states              = int(self.get_hyperparam().get_value(self._hp_ids[0]))
         self._algo              = p_algo
         
-        self.exploration         = 1
         self.performance_map     = torch.zeros((2, num_states, num_states))
         x                        = torch.linspace(1/(num_states*2), 1-(1/(num_states*2)), num_states)
         self.grid_x, self.grid_y = torch.meshgrid(x, x)
+        self.exploration         = 1
         self._counter            = 0
         self._current_ep         = 1
         
-        if self._algo != self.ALG_Vanilla_SbPG:
+        if self._algo != self.ALG_SbPG_BR:
             self.map_nxt_action  = torch.zeros((num_states, num_states))
         
         
 ## -------------------------------------------------------------------------------------------------
     def _init_hyperparam(self):
-        # 1.2 Declare hyperparameters with unique id, names, and data type   
+        
         self._hyperparam_space.add_dim(HyperParam('num_states','Z'))
         self._hyperparam_space.add_dim(HyperParam('exploration_decay','R'))
-        self._hyperparam_space.add_dim(HyperParam('alpha','R'))                 # gradient-based learning
-        self._hyperparam_space.add_dim(HyperParam('OU_noise','R'))              # gradient-based learning
-        self._hyperparam_space.add_dim(HyperParam('kick_off_eps','Z'))          # gradient-based learning
+        self._hyperparam_space.add_dim(HyperParam('alpha','R'))             # GB learning
+        self._hyperparam_space.add_dim(HyperParam('OU_noise','R'))          # GB learning
+        self._hyperparam_space.add_dim(HyperParam('kick_off_eps','Z'))      # GB learning
         self._hyperparam_space.add_dim(HyperParam('cycles_per_ep','Z'))
         self._hyperparam_space.add_dim(HyperParam('smoothing','R'))
         self._hyperparam_space.add_dim(HyperParam('ep_max','Z'))
-        self._hyperparam_space.add_dim(HyperParam('beta','R'))                 # gradient-based learning (Method 2)
+        self._hyperparam_space.add_dim(HyperParam('beta','R'))              # GB learning (Momentum)
         self._hyperparam_tuple = HyperParamTuple(self._hyperparam_space)
         
-        # 1.3 Set the hyperparameter with a default value 
         ids_ = self._hyperparam_tuple.get_dim_ids()
         self._hyperparam_tuple.set_value(ids_[0], 40)
         self._hyperparam_tuple.set_value(ids_[1], 0.55**(2/1000))
-        self._hyperparam_tuple.set_value(ids_[2], 1.0) # gb method 1 = 1.0, method 2 = 0.5
+        self._hyperparam_tuple.set_value(ids_[2], 1.0)
         self._hyperparam_tuple.set_value(ids_[3], 0)
         self._hyperparam_tuple.set_value(ids_[4], 0)
         self._hyperparam_tuple.set_value(ids_[5], 1000)
@@ -138,36 +149,31 @@ class SbPG(Policy):
         
 ## -------------------------------------------------------------------------------------------------
     def get_hyperparam(self) -> HyperParamTuple:
+
         return self._hyperparam_tuple
 
 
 ## -------------------------------------------------------------------------------------------------
-    def compute_action(self, p_state: State) -> Action:
+    def compute_action(self, p_state:State) -> Action:
         
-        if self._algo == self.ALG_Vanilla_SbPG:
-            return self.compute_action_1(p_state)
+        if self._algo == self.ALG_SbPG_BR:
+            return self.compute_action_br(p_state)
         else:
-            return self.compute_action_2(p_state)
+            return self.compute_action_gb(p_state)
 
 
 ## -------------------------------------------------------------------------------------------------
-    def compute_action_1(self, p_state: State) -> Action:
+    def compute_action_br(self, p_state:State) -> Action:
         
-        my_action_values    = np.zeros(self._action_space.get_num_dim())
-        actual_states       = p_state.get_values()
-        ids_                = self.get_hyperparam().get_dim_ids()
+        my_action_values        = np.zeros(self._action_space.get_num_dim())
+        actual_states           = p_state.get_values()
         
-        x_disc, y_disc      = self._discretization(
-            actual_states[0],
-            actual_states[1]
-            )
-        
-        if self._counter == self.get_hyperparam().get_value(ids_[5]):
+        if self._counter == self.get_hyperparam().get_value(self._hp_ids[5]):
             self._counter       = 0
             self.exploration    = 1
             self._current_ep    += 1
         
-        if self._current_ep == self.get_hyperparam().get_value(ids_[7]):
+        if self._current_ep == self.get_hyperparam().get_value(self._hp_ids[7]):
             self.exploration    = 0
             
         if random.uniform(0, 1) < self.exploration:
@@ -187,41 +193,31 @@ class SbPG(Policy):
 
 
 ## -------------------------------------------------------------------------------------------------
-    def compute_action_2(self, p_state: State) -> Action:
+    def compute_action_gb(self, p_state:State) -> Action:
         
-        my_action_values    = np.zeros(self._action_space.get_num_dim())
-        actual_states       = p_state.get_values()
-        ids_                = self.get_hyperparam().get_dim_ids()
-        
-        x_disc, y_disc      = self._discretization(
-            actual_states[0],
-            actual_states[1]
-            )
+        my_action_values        = np.zeros(self._action_space.get_num_dim())
+        actual_states           = p_state.get_values()
+        x_disc, y_disc          = self._discretization(actual_states[0],actual_states[1])
     
-        if self._counter == self.get_hyperparam().get_value(ids_[5]):
+        if self._counter == self.get_hyperparam().get_value(self._hp_ids[5]):
             self._counter       = 0
             self.exploration    = 1
             self._current_ep    += 1
         
-        if self._current_ep == self.get_hyperparam().get_value(ids_[7]):
+        if self._current_ep == self.get_hyperparam().get_value(self._hp_ids[7]):
             self.exploration    = 0
             
         if random.uniform(0, 1) < self.exploration:
-            if self._current_ep <= self.get_hyperparam().get_value(ids_[4]):
+            if self._current_ep <= self.get_hyperparam().get_value(self._hp_ids[4]):
                 my_action_values[0] = random.uniform(0, 1)
             else:
-                nxt_action = self.map_nxt_action[x_disc, y_disc].item()
-                ou_noise = self.get_hyperparam().get_value(ids_[3])
+                nxt_action          = self.map_nxt_action[x_disc, y_disc].item()
+                ou_noise            = self.get_hyperparam().get_value(self._hp_ids[3])
                 my_action_values[0] = max(0,min(1,random.uniform(nxt_action-ou_noise,nxt_action+ou_noise)))
         else:
-            # if self.exploration != 0 and self.performance_map[0, x_disc, y_disc] and actual_states[0] <= 0.8:
-            #     my_action_values[0] = random.uniform(0, 1)
-            # else:
-            #     my_action_values[0] = self.interpolate_maps(actual_states[0], actual_states[1])
-            my_action_values[0] = self.interpolate_maps(actual_states[0], actual_states[1])
+            my_action_values[0]     = self.interpolate_maps(actual_states[0], actual_states[1])
             
         _id = self._action_space.get_dim_ids()[0]
-        
         if self._action_space.get_dim(_id).get_base_set() == "Z":
             if my_action_values[0] >= 0.5:
                 my_action_values[0] = 1
@@ -234,16 +230,16 @@ class SbPG(Policy):
 ## -------------------------------------------------------------------------------------------------
     def _adapt(self, **p_kwargs) -> bool:
         
-        if self._algo == self.ALG_Vanilla_SbPG:
-            return self._adapt_1(p_kwargs)
-        elif self._algo == self.ALG_GB_SbPG_METHOD_1:
-            return self._adapt_2(p_kwargs)
-        elif self._algo == self.ALG_GB_SbPG_METHOD_2:
-            return self._adapt_3(p_kwargs)
+        if self._algo == self.ALG_SbPG_BR:
+            return self._adapt_br(p_kwargs)
+        elif self._algo == self.ALG_SbPG_GB:
+            return self._adapt_gb(p_kwargs)
+        elif self._algo == self.ALG_SbPG_GB_MOM:
+            return self._adapt_gb_mom(p_kwargs)
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _adapt_1(self, p_kwargs) -> bool:
+    def _adapt_br(self, p_kwargs) -> bool:
         
         prev_states = p_kwargs["p_sars_elem"].get_data()["state"].get_values()
         
@@ -258,15 +254,14 @@ class SbPG(Policy):
             self.performance_map[1,x_disc,y_disc] = rwd
             self.performance_map[0,x_disc,y_disc] = p_kwargs["p_sars_elem"].get_data()["action"].get_sorted_values().item()
         
-        ids_ = self.get_hyperparam().get_dim_ids()
-        self.exploration *= self.get_hyperparam().get_value(ids_[1])
+        self.exploration *= self.get_hyperparam().get_value(self._hp_ids[1])
         self._counter += 1
         
         return True
 
 
 ## -------------------------------------------------------------------------------------------------
-    def _adapt_2(self, p_kwargs) -> bool:
+    def _adapt_gb(self, p_kwargs) -> bool:
         
         prev_states = p_kwargs["p_sars_elem"].get_data()["state"].get_values()
         
@@ -289,17 +284,16 @@ class SbPG(Policy):
             self.performance_map[0, x_disc, y_disc] = act
             self.performance_map[1, x_disc, y_disc] = util
             
-        ids_ = self.get_hyperparam().get_dim_ids()
-        lr_gradient = self.get_hyperparam().get_value(ids_[2])
+        lr_gradient = self.get_hyperparam().get_value(self._hp_ids[2])
         self.map_nxt_action[x_disc, y_disc] = min(1,max(0,act)+(lr_gradient*gradient))
         
-        self.exploration *= self.get_hyperparam().get_value(ids_[1])
+        self.exploration *= self.get_hyperparam().get_value(self._hp_ids[1])
         self._counter += 1
         
         return True
 
 ## -------------------------------------------------------------------------------------------------
-    def _adapt_3(self, p_kwargs) -> bool:
+    def _adapt_gb_mom(self, p_kwargs) -> bool:
         
         prev_states = p_kwargs["p_sars_elem"].get_data()["state"].get_values()
         
@@ -322,16 +316,15 @@ class SbPG(Policy):
             self.performance_map[0, x_disc, y_disc] = act
             self.performance_map[1, x_disc, y_disc] = util
             
-        ids_ = self.get_hyperparam().get_dim_ids()
-        lr_gradient = self.get_hyperparam().get_value(ids_[2])
+        lr_gradient = self.get_hyperparam().get_value(self._hp_ids[2])
         if self.map_nxt_action[x_disc, y_disc] == 0:
             self.map_nxt_action[x_disc, y_disc] = min(1,max(0,act)+(lr_gradient*gradient))
         else:
             prev_gradient = self.map_nxt_action[x_disc, y_disc]
-            gradient = self.get_hyperparam().get_value(ids_[8])*prev_gradient + (1-self.get_hyperparam().get_value(ids_[8]))*gradient
+            gradient = self.get_hyperparam().get_value(self._hp_ids[8])*prev_gradient + (1-self.get_hyperparam().get_value(self._hp_ids[8]))*gradient
             self.map_nxt_action[x_disc, y_disc] = min(1,max(0,act)+(lr_gradient*gradient))
             
-        self.exploration *= self.get_hyperparam().get_value(ids_[1])
+        self.exploration *= self.get_hyperparam().get_value(self._hp_ids[1])
         self._counter += 1
         
         return True
@@ -340,8 +333,7 @@ class SbPG(Policy):
 ## -------------------------------------------------------------------------------------------------        
     def _discretization(self, x_fill_level, y_fill_level):
                
-        ids_        = self.get_hyperparam().get_dim_ids()
-        num_states  = self.get_hyperparam().get_value(ids_[0])
+        num_states  = self.get_hyperparam().get_value(self._hp_ids[0])
         x_disc      = min(m.floor(x_fill_level*num_states), int(num_states-1))
         y_disc      = min(m.floor(y_fill_level*num_states), int(num_states-1))
         
@@ -350,12 +342,11 @@ class SbPG(Policy):
     
 ## -------------------------------------------------------------------------------------------------    
     def interpolate_maps(self, pos_x, pos_y):
-        
-        ids_ = self.get_hyperparam().get_dim_ids()
-        
+                
         distances = torch.sqrt(((pos_x-self.grid_x)**2) + ((pos_y-self.grid_y)**2))
         distances[distances == 0] = 0.0001
-        weights = 1/((distances**2)+self.get_hyperparam().get_value(ids_[6]))
+        weights = 1/((distances**2)+self.get_hyperparam().get_value(self._hp_ids[6]))
         DistancesTotal = sum(sum(weights))
         outputs = (weights/DistancesTotal)*self.performance_map[0,:,:]
+
         return sum(sum(outputs))
