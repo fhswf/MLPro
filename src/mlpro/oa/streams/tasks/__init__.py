f0f95e277c1be68b42d401d0362f2c67100a8b92
detlefarend <detlef@arend-privat.net>
2025-05-28 08:54:21 +0200

    OA: Boundary detector - Improvement of reverse adaptation #1118

--- src/mlpro/oa/streams/tasks/__init__.py
+++ src/mlpro/oa/streams/tasks/__init__.py
@@ -1,5 +1,4 @@
-from mlpro.oa.streams.tasks.boundarydetector import BoundaryDetector
-from mlpro.oa.streams.tasks.normalizers import *
+from mlpro.oa.streams.tasks.boundarydetectors import *
 from mlpro.oa.streams.tasks.clusteranalyzers import *
 from mlpro.oa.streams.tasks.anomalydetectors import *
 from mlpro.oa.streams.tasks.driftdetectors import *
