eb834e3fe04ab777a2c47109f13149a728ff9d0e
detlefarend <detlef@arend-privat.net>
2025-06-02 19:04:57 +0200

    OA: Basics of online adaptive closed-loop control #1047

--- src/mlpro/oa/control/controllers/wrapper_rl.py
+++ src/mlpro/oa/control/controllers/wrapper_rl.py
@@ -13,7 +13,8 @@
 ## --                                - implementation of plot methods
 ## .-                                - redefintion of method assign_so()
 ## -- 2024-12-06  0.4.1     DA       Method OAControllerRL.__init__(): handling of the own name
-## -- 2025-06-02  0.4.2     DA       Bugfix in OAControllerRL.__init__()
+## -- 2025-06-02  0.4.2     DA       - Bugfix in OAControllerRL.__init__()
+## --                                - Plot methods of OAControllerRL disabled
 ## -------------------------------------------------------------------------------------------------
 
 """
@@ -165,46 +166,46 @@ class OAControllerRL (OAController):
         return adapted
     
 
-## -------------------------------------------------------------------------------------------------
-    def _init_plot_2d(self, p_figure, p_settings):
-        return self._rl_policy._init_plot_2d(p_figure, p_settings)
+# ## -------------------------------------------------------------------------------------------------
+#     def _init_plot_2d(self, p_figure, p_settings):
+#         return self._rl_policy._init_plot_2d(p_figure, p_settings)
     
 
-## -------------------------------------------------------------------------------------------------
-    def _init_plot_3d(self, p_figure, p_settings):
-        return self._rl_policy._init_plot_3d(p_figure, p_settings)
+# ## -------------------------------------------------------------------------------------------------
+#     def _init_plot_3d(self, p_figure, p_settings):
+#         return self._rl_policy._init_plot_3d(p_figure, p_settings)
     
 
-## -------------------------------------------------------------------------------------------------
-    def _init_plot_nd(self, p_figure, p_settings):
-        return self._rl_policy._init_plot_nd(p_figure, p_settings)
+# ## -------------------------------------------------------------------------------------------------
+#     def _init_plot_nd(self, p_figure, p_settings):
+#         return self._rl_policy._init_plot_nd(p_figure, p_settings)
     
 
-## -------------------------------------------------------------------------------------------------
-    def _update_plot_2d(self, p_settings, p_inst, **p_kwargs):
-        return self._rl_policy._update_plot_2d(p_settings, p_inst, **p_kwargs)
+# ## -------------------------------------------------------------------------------------------------
+#     def _update_plot_2d(self, p_settings, p_inst, **p_kwargs):
+#         return self._rl_policy._update_plot_2d(p_settings, p_inst, **p_kwargs)
     
 
-## -------------------------------------------------------------------------------------------------
-    def _update_plot_3d(self, p_settings, p_inst, **p_kwargs):
-        return self._rl_policy._update_plot_3d(p_settings, p_inst, **p_kwargs)
+# ## -------------------------------------------------------------------------------------------------
+#     def _update_plot_3d(self, p_settings, p_inst, **p_kwargs):
+#         return self._rl_policy._update_plot_3d(p_settings, p_inst, **p_kwargs)
     
 
-## -------------------------------------------------------------------------------------------------
-    def _update_plot_nd(self, p_settings, p_inst, **p_kwargs):
-        return self._rl_policy._update_plot_nd(p_settings, p_inst, **p_kwargs)
+# ## -------------------------------------------------------------------------------------------------
+#     def _update_plot_nd(self, p_settings, p_inst, **p_kwargs):
+#         return self._rl_policy._update_plot_nd(p_settings, p_inst, **p_kwargs)
     
 
-## -------------------------------------------------------------------------------------------------
-    def _remove_plot_2d(self):
-        return self._rl_policy._remove_plot_2d()
+# ## -------------------------------------------------------------------------------------------------
+#     def _remove_plot_2d(self):
+#         return self._rl_policy._remove_plot_2d()
     
 
-## -------------------------------------------------------------------------------------------------
-    def _remove_plot_3d(self):
-        return self._rl_policy._remove_plot_3d()
+# ## -------------------------------------------------------------------------------------------------
+#     def _remove_plot_3d(self):
+#         return self._rl_policy._remove_plot_3d()
     
 
-## -------------------------------------------------------------------------------------------------
-    def _remove_plot_nd(self):
-        return self._rl_policy._remove_plot_nd()
+# ## -------------------------------------------------------------------------------------------------
+#     def _remove_plot_nd(self):
+#         return self._rl_policy._remove_plot_nd()
