437fadb5c4bb399ce5ebe2e85b6ad323776a4391
Devindi97 <devindi.1207@gmail.com>
2025-06-08 19:42:19 +0200

    New cluster based drift detectors based on generic_single_movement #1207

--- src/mlpro/oa/streams/tasks/changedetectors/driftdetectors/drifts/clusterbased/size.py
+++ src/mlpro/oa/streams/tasks/changedetectors/driftdetectors/drifts/clusterbased/size.py
@@ -1,17 +1,17 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- Project : MLPro - The integrative middleware framework for standardized machine learning
 ## -- Package : mlpro.oa.tasks.driftdetectors.drifts.clusterbased
-## -- Module  : deformation.py
+## -- Module  : size.py
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
-## -- 2025-02-13  1.0.0     DA       Creation
+## -- 2025-06-06  1.0.0     DS       Creation
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 1.0.0 (2025-02-13)
+Ver. 1.0.0 (2025-06-06)
 
-This module provides a sub-typ of class DriftCB related to cluster deformation.
+This module provides a sub-typ of class DriftCB related to cluster size.
 """
 
 from mlpro.oa.streams.tasks.changedetectors.driftdetectors.drifts.clusterbased.basics import DriftCB
@@ -21,9 +21,9 @@ from mlpro.oa.streams.tasks.changedetectors.driftdetectors.drifts.clusterbased.b
 
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class DriftCBDeformation (DriftCB):
+class DriftCBSize (DriftCB):
     """
-    Sub-type indicating the begin or end of a cluster deformation.
+    Sub-type indicating the begin or end of a cluster size.
     """
 
     C_PLOT_ACTIVE   = True
