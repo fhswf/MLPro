9c49180d0736a0a94b738cb8a13dca17036ebd44
Devindi97 <devindi.1207@gmail.com>
2025-07-15 12:01:17 +0200

    OA: New systematics for generic cluster-based anomaly detection Fixes #1138

--- src/mlpro/oa/streams/helpers/__init__.py
+++ src/mlpro/oa/streams/helpers/__init__.py
@@ -1,2 +1,3 @@
 from mlpro.oa.streams.helpers.oa_observer import OAObserver
-from mlpro.oa.streams.helpers.change_observer import ChangeObserver, ChangeObserverCB
+from mlpro.oa.streams.helpers.change_observer import ChangeObserver
+from mlpro.oa.streams.helpers.change_observer_cb import ChangeObserverCB
