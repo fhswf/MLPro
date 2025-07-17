c4ae2f71b5df6a2fc3a26b11e812090b022b9a1d
detlefarend <detlef@arend-privat.net>
2025-07-17 16:01:09 +0200

    Refact: API cleanup Fixes #1248

--- src/mlpro/rl/__init__.py
+++ src/mlpro/rl/__init__.py
@@ -1,1 +1,10 @@
+"""
+### MLPro-RL – Reinforcement Learning (`mlpro.rl`)
+
+---
+
+**Learn more:** `MLPro-RL documentation <https://mlpro.readthedocs.io/en/latest/content/03_machine_learning/mlpro_rl/main.html>`_
+
+"""
+
 from mlpro.rl.models import *
