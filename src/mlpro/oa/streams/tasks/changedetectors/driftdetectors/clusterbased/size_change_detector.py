f83067122dbda75405ff5758571f530af1defa11
Devindi97 <devindi.1207@gmail.com>
2025-07-20 17:37:07 +0200

    New cluster based drift detectors based on generic_single_movement Fixes #1207

--- src/mlpro/oa/streams/tasks/changedetectors/driftdetectors/clusterbased/size_change_detector.py
+++ src/mlpro/oa/streams/tasks/changedetectors/driftdetectors/clusterbased/size_change_detector.py
@@ -55,7 +55,7 @@ class DriftDetectorCBGenSizeChange (DriftDetectorCBGenSingleGradient):
                   p_logging=Log.C_LOG_ALL,
                   p_drift_buffer_size : int = 100,
                   p_thrs_inst : int = 0,
-                  p_thrs_cluster : int = 1 ):
+                  p_thrs_clusters : int = 1 ):
 
         super().__init__( p_clusterer = p_clusterer,
                           p_property = cprop_size_geo,
@@ -70,4 +70,4 @@ class DriftDetectorCBGenSizeChange (DriftDetectorCBGenSingleGradient):
                           p_logging = p_logging,
                           p_drift_buffer_size = p_drift_buffer_size,
                           p_thrs_inst = p_thrs_inst,
-                          p_thrs_cluster = p_thrs_cluster )
+                          p_thrs_clusters = p_thrs_clusters )
